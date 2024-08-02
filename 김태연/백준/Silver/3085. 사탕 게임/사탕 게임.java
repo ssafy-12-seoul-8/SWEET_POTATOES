211d97a8ce284acb87443380116ecb49779adfc4
skybory <154962211+skybory@users.noreply.github.com>
2024-08-02 21:15:18 +0900

    [Silver II] Title: 사탕 게임, Time: 244 ms, Memory: 17868 KB -SsafyHub

--- 김태연/백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 김태연/백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -21,40 +21,43 @@ class Main {
             }
         }
 
-        // 브루트포스 방법을 사용하여 모든 경우의 수를 확인
+        // 브루트포스 사용
+        // 1. 오른쪽으로 바꾸기
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N - 1; j++) {
-                // 오른쪽과 자리바꿈
+                // 스왑 (i,j) <-> (i,j+1)
                 swap(i, j, i, j + 1);
                 max = Math.max(max, findMax());
-                swap(i, j, i, j + 1); // 원위치
+                swap(i, j, i, j + 1); // 제자리로
             }
         }
 
         for (int i = 0; i < N - 1; i++) {
             for (int j = 0; j < N; j++) {
-                // 아래쪽과 자리바꿈
+                // 아래랑 스왑
                 swap(i, j, i + 1, j);
                 max = Math.max(max, findMax());
-                swap(i, j, i + 1, j); // 원위치
+                swap(i, j, i + 1, j); // 원위치시킴
             }
         }
 
         System.out.println(max);
     }
 
-    static void swap(int x1, int y1, int x2, int y2) {
-        char temp = arr[x1][y1];
-        arr[x1][y1] = arr[x2][y2];
-        arr[x2][y2] = temp;
+    // 위치바꾸기
+    static void swap(int bx, int by, int ax, int ay) {
+        char temp = arr[bx][by];
+        arr[bx][by] = arr[ax][ay];
+        arr[ax][ay] = temp;
     }
 
+    // 최대값 찾기
     static int findMax() {
         int max = 0;
 
         // 행 확인
         for (int i = 0; i < N; i++) {
-            int cnt = 1;
+            int cnt = 1;    // 이부분 첨에 틀렸음(저번에도 한번 틀림)
             for (int j = 1; j < N; j++) {
                 if (arr[i][j] == arr[i][j - 1]) {
                     cnt++;
