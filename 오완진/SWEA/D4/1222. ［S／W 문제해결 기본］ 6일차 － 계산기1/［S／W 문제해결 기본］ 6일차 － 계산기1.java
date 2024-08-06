9604927aff69919fc1000e9b9d80804ff64c2b39
WJ <130027416+OiPKL@users.noreply.github.com>
2024-08-06 12:24:34 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 145 ms, Memory: 20,188 KB -SsafyHub

---
+++ 오완진/SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for (int t = 1; t <= 10; t++) {
+			
+			int N = sc.nextInt();
+			sc.nextLine();
+			String expression = sc.nextLine();
+			int result = evaluate(expression);
+			
+			System.out.println("#" + t + " " + result);
+		}
+	}
+	
+	static int evaluate(String expression) {
+		String postfix = infixToPostfix(expression);
+		return evalPostfix(postfix);
+	}
+	
+	static String infixToPostfix(String infix) {
+		
+		String postfix = "";
+		Stack<Character> stack = new Stack<>();
+		
+		for (int i = 0; i < infix.length(); i++) {
+			char c = infix.charAt(i);
+			
+			if ('0' <= c && c <= '9') {
+				// 숫자 -> 출력
+				postfix += c;
+			} else {
+				// + -> 스택
+				stack.push(c);
+			}
+		}
+		
+		// 스택 비우기
+		while (!stack.isEmpty()) {
+			postfix += stack.pop();
+		}
+
+		return postfix;
+	}
+	
+	static int evalPostfix(String postfix) {
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		for (int i = 0; i < postfix.length(); i++) {
+			char c = postfix.charAt(i);
+			
+			if ('0' <= c && c <= '9') {
+				stack.push(c - '0');
+			} else {
+				int num2 = stack.pop();
+				int num1 = stack.pop();
+				int result;
+				
+				result = num1 + num2;
+				
+				stack.push(result);
+			}
+		}
+		
+		return stack.pop();
+	}
+	
+}
