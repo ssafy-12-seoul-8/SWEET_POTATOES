03e848265b6bb82024758c6e38221dbbd0b088c7
ciocio <tmddusleo@icloud.com>
2024-08-06 22:51:07 +0900

    [level 1] Title: 신규 아이디 추천, Time: 27.07 ms, Memory: 81.5 MB -SsafyHub

--- 이승연/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 이승연/프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -14,15 +14,18 @@ class Solution {
         
         // 1단계 new_id의 모든 대문자를 대응되는 소문자로 치환합니다.
         answer = new_id.toLowerCase();
+        
         // 2단계 new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
         answer = answer.replaceAll("[^a-z0-9._-]", "");
+        
         // 3단계 new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
         answer = answer.replaceAll("\\.{2,}", ".");
+        
         // 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
         answer = answer.replaceAll("^\\.|\\.$", "");
         
         // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
-        if (answer.length() == 0) {
+        if (answer.isEmpty()) {
             answer += 'a';
         }
         
@@ -36,6 +39,7 @@ class Solution {
             answer = answer.substring(0, 14);
         }
         
+        // 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
         if (answer.length() <= 2) {
             char c = answer.charAt(answer.length() - 1);
             
