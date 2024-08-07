59c1d2eac6f63131272fe27607cf9c4316c1defd
skybory <154962211+skybory@users.noreply.github.com>
2024-08-07 11:39:37 +0900

    [D3] Title: 준홍이의 카드놀이, Time: 117 ms, Memory: 19,128 KB -SsafyHub

--- 김태연/SWEA/D3/7102. 준홍이의 카드놀이/준홍이의 카드놀이.java
+++ 김태연/SWEA/D3/7102. 준홍이의 카드놀이/준홍이의 카드놀이.java
@@ -1,19 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+		
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
 
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 		
+			st = new StringTokenizer(br.readLine());
 			int N,M;
-			N = sc.nextInt();	// 첫 번째 큐 길이
-			M = sc.nextInt();	// 두 번째 큐 길이
+			N = Integer.parseInt(st.nextToken());	// 첫 번째 큐 길이
+			M = Integer.parseInt(st.nextToken());	// 두 번째 큐 길이
 			int sum = 0;	
 			int max = 0;
 			int[] count = new int[N+M+1];		// N 이 6, M 이 4일때, N+M = 10 까지 올라감. 그러면 sum = 10까지 올라가고, count[10]++ 이 최대인데 왜 11이 들어감?
@@ -25,14 +32,13 @@ class Solution
 					max = Math.max(max, count[sum]);	// 최대값 업데이트
 				}
 			}
-			
-			System.out.print("#" + test_case);
+			sb.append("#").append(test_case);
 			
 			for (int i=0; i<count.length; i++) {
-				if (count[i] == max) System.out.print(" " + i);
+				if (count[i] == max) sb.append(" ").append(i);
 			}
-			System.out.println();
-			
+			sb.append("\n");
 		}
+		System.out.println(sb);
 	}
 }
