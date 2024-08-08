ae422d4752eaee4b05cf5e9608bf006b4e9b8b4e
skybory <154962211+skybory@users.noreply.github.com>
2024-08-08 16:48:51 +0900

    [D3] Title: 농작물 수확하기, Time: 293 ms, Memory: 28,828 KB -SsafyHub

--- 김태연/SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ 김태연/SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,44 +1,42 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 class Solution {
-    public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt(); // 1
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T;
+		T = Integer.parseInt(br.readLine()); // 1
+		StringBuilder sb = new StringBuilder();
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
 
-    for (int test_case = 1; test_case <= T; test_case++) {
+			int N;	
+			N = Integer.parseInt(br.readLine());	// 5
+			int[][] arr = new int[N][N];
+			String line;
+			for (int i = 0; i < N; i++) {
+				line = br.readLine();		// 14054 * 5
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = line.charAt(j) - '0';
+				}
+			}
 
-        int N;
-        N = sc.nextInt();
-        int[][] arr = new int[N][N];
-        String line;
-        for (int i = 0; i < N; i++) {
-            line = sc.next();
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = line.charAt(j) - '0';
-            }
-        }
+			// 입력 다 받았고, 합계 구해야함
+			// 함수로 구하면 쉬울것같음
 
-        // 입력 다 받았고, 합계 구해야함
-        // 함수로 구하면 쉬울것같음
+			int L = N / 2;
+			int sum = 0;
+			// (0,0) ~ (6,6)
+			for (int y = 0; y < N; y++) {
+				for (int x = 0; x < N; x++) {
 
-        int L = N/2;
-//        System.out.println("L 값은 : " + L);
-//        System.out.println("L+N 값은 : " + (L+N));
-        int sum = 0;
-        // (0,0) ~ (6,6)
-        for (int y = 0; y < N; y++) {
-            for (int x = 0; x < N; x++) {
-
-                if (y <= x + L && y >= x - L && y <= -x +L + N - 1 && y >= -x + L) {
-//                    System.out.println(x + " , " + y + " 를 포함합니다");
-                    sum += arr[x][y];
-//                    System.out.println("sum : " + sum);
-                }
-            }
-        }
-
-        System.out.println("#" + test_case + " " + sum);
-    }
-}
+					if (y <= x + L && y >= x - L && y <= -x + L + N - 1 && y >= -x + L) {
+						sum += arr[x][y];
+					}
+				}
+			}
+			sb.append("#").append(test_case).append(" ").append(sum).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
