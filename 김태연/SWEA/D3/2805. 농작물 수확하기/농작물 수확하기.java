149c10c98a977a3f270e266f87f222361305342f
skybory <154962211+skybory@users.noreply.github.com>
2024-08-09 09:27:42 +0900

    [D3] Title: 농작물 수확하기, Time: 293 ms, Memory: 28,828 KB -SsafyHub

--- 김태연/SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ 김태연/SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,42 +1,48 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-class Solution {
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T;
-		T = Integer.parseInt(br.readLine()); // 1
-		StringBuilder sb = new StringBuilder();
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
 		
 		for (int test_case = 1; test_case <= T; test_case++) {
+			
+			int N = sc.nextInt();	// 5
+			
 
-			int N;	
-			N = Integer.parseInt(br.readLine());	// 5
-			int[][] arr = new int[N][N];
-			String line;
-			for (int i = 0; i < N; i++) {
-				line = br.readLine();		// 14054 * 5
-				for (int j = 0; j < N; j++) {
-					arr[i][j] = line.charAt(j) - '0';
+			int[][] board = new int[N][N];
+		
+			for (int r=0; r < N; r++) {
+				String line = sc.next();
+				for (int c = 0; c < N; c++) {
+					board[r][c] = line.charAt(c) - '0';
 				}
 			}
-
-			// 입력 다 받았고, 합계 구해야함
-			// 함수로 구하면 쉬울것같음
-
-			int L = N / 2;
+			
 			int sum = 0;
-			// (0,0) ~ (6,6)
-			for (int y = 0; y < N; y++) {
-				for (int x = 0; x < N; x++) {
-
-					if (y <= x + L && y >= x - L && y <= -x + L + N - 1 && y >= -x + L) {
-						sum += arr[x][y];
-					}
+			int start = N / 2;
+			int end = N / 2;
+			
+			
+			for (int r = 0; r < N; r++) {
+				for (int c = start; c <= end; c++) {
+					sum += board[r][c];
+				}
+				
+				if ( r < N / 2) {
+					start--;
+					end++;
+				} else {
+					start++;
+					end--;
 				}
 			}
-			sb.append("#").append(test_case).append(" ").append(sum).append("\n");
+			
+//			System.out.println(Arrays.deepToString(board));
+
+			System.out.println("#" + test_case + " " + sum);
 		}
-		System.out.println(sb);
 	}
 }
