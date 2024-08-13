fb256290f26dd66cb3ebe1709f2838bb70e14b10
skybory <154962211+skybory@users.noreply.github.com>
2024-08-13 17:01:51 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 178 ms, Memory: 27,440 KB -SsafyHub

--- 김태연/SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
+++ 김태연/SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -1,65 +1,87 @@
 import java.util.Scanner;
-
+ 
 class Solution {
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T = 10;
-
-		for (int test_case = 1; test_case <= T; test_case++) {
-
-			int N = sc.nextInt(); // 193
-			int[] password = new int[N];
-
-			// 원본 패스워드 받기
-			for (int i = 0; i < N; i++) {
-				password[i] = sc.nextInt();
-			}
-
-			int M = sc.nextInt(); // 초록색 숫자 (10~20)
-
-			// 명령문 반복
-			for (int i = 0; i < M; i++) {
-				String command = sc.next(); // I 버림
-				switch (command.charAt(0)) {
-
-				case 'I': {
-					int idx = sc.nextInt(); // idx 번호 받음 // 19
-					int K = sc.nextInt(); // 길이 K 의 숫자 배열 생성 // 4
-
-					int[] temp = new int[K];
-					for (int j = 0; j < K; j++) {
-						temp[j] = sc.nextInt();
-					}
-
-					for (int j = N - 1; j >= idx + K; j--) {
-						// 원래 있던 값을 먼저 옮겨야함. 이건 밀려쓰는 구문이라, 반드시 9번인덱스 까지 시행해야함.
-						password[j] = password[j - K]; // answer[6] = answer[1] , answer[7] = answer[2] ...
-					}
-
-					for (int j = 0; j < K; j++) {
-						// 삽입 구문
-						if (idx + j < N)
-							password[idx + j] = temp[j]; // password[1] = new Password[0];
-					}
-				}
-					break;
-				case 'D': {
-					int idx = sc.nextInt();
-					int K = sc.nextInt();
-					for (int j = idx; j < N-K; j++) {
-						password[j] = password[j + K];
-					}
-				}
-					break;
-				}
-			}
-
-			System.out.print("#" + test_case);
-			for (int i = 0; i < 10; i++) {
-				System.out.print(" " + password[i]);
-			}
-			System.out.println();
-		}
-	}
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = 10;
+ 
+        for (int test_case = 1; test_case <= T; test_case++) {
+ 
+            int N = sc.nextInt(); // 193
+            int[] password = new int[N];
+ 
+            // 원본 패스워드 받기
+            for (int i = 0; i < N; i++) {
+                password[i] = sc.nextInt();
+            }
+ 
+            int M = sc.nextInt(); // 초록색 숫자 (10~20)
+ 
+            // 명령문 반복
+            for (int i = 0; i < M; i++) {
+                String command = sc.next(); // I 버림
+                switch (command.charAt(0)) {
+ 
+                case 'I': {
+                    int idx = sc.nextInt(); // idx 번호 받음 // 19
+                    int K = sc.nextInt(); // 길이 K 의 숫자 배열 생성 // 4
+ 
+                    int[] insertedPassword = new int[password.length + K];
+                    
+                    for (int j = 0; j< idx; j++) {
+                    	insertedPassword[j] = password[j];	// password[idx-1] 까지 복사했음
+                    }
+                    
+                    for (int j = idx; j < idx+K; j++) {
+                    	insertedPassword[j] = sc.nextInt();
+                    }
+ 
+                    for (int j = idx + K; j < password.length + K; j++) {
+                        insertedPassword[j] = password[j - K];	// password[idx] 부터 복사해야함
+                    }
+                    
+                    password = insertedPassword;
+                }
+                    break;
+                case 'D': {
+                    int idx = sc.nextInt();
+                    int K = sc.nextInt();
+                    
+                    // 기존의 길이에서 K 만큼 빠짐
+                    int[] deletedPassword = new int[password.length - K];
+                    
+                    // 삭제하는 구간 앞 부분 복사
+//                    int minIdx = Math.min(test_case, password.length);	// 이 부분 손봐보자.
+                    for (int j=0; j<idx; j++) {
+                    	deletedPassword[j] = password[j];
+                    }
+                    
+                    // 삭제하는 구간 뒷 부분 복사
+                    for (int j = idx; j < password.length-K; j++) {
+                    	deletedPassword[j] = password[j+K];
+                    }
+                    
+                    password = deletedPassword;	// 짧아진 패스워드로 대체
+                    
+                }
+                    break;
+ 
+                }
+            }
+ 
+            System.out.print("#" + test_case);
+            for (int i = 0; i < 10; i++) {
+                System.out.print(" " + password[i]);
+            }
+            System.out.println();
+        }
+    }
 }
+
+/*
+21
+750997 285496 683627 815415 174006 928490 179846 725245 914065 999222 898388 533467 206884 107381 750997 815415 174006 928490 533467 206884 107381 
+2
+D 1 2 D 4 5
+ */
