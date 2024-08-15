1a62520e6eb2231f9c523c1db15c2a8757e6dcc7
Ryuharam <dlfnadl97@naver.com>
2024-08-16 08:13:19 +0900

    [D3] Title: 신뢰, Time: 214 ms, Memory: 44,268 KB -SsafyHub

---
+++ 유하람/SWEA/D3/10761. 신뢰/README.md
@@ -0,0 +1,15 @@
+# [D3] 신뢰 - 10761 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXSVc1TqEAYDFAQT) 
+
+### 성능 요약
+
+메모리: 44,268 KB, 시간: 214 ms, 코드길이: 1,497 Bytes
+
+### 제출 일자
+
+2024-08-16 08:13
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ 유하람/SWEA/D3/10761. 신뢰/신뢰.java
@@ -0,0 +1,89 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static class Robot{
+		int idx;
+		int time;
+		
+		
+		public Robot(int idx, int time) {
+			this.idx = idx;
+			this.time = time;
+		}
+		
+	}
+    
+    public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        
+		for(int t = 1; t <= T; t++)
+		{
+            // 명령어의 수
+			int n = sc.nextInt();
+			
+			Robot O = new Robot(1,0);
+			
+			Robot B = new Robot(1,0);
+			
+			// 시간
+			int time = 0;
+			
+			for(int i=0 ; i<n ;i++) {
+				
+				String s = sc.next();
+				
+				if(s.equals("O")) {
+					
+					int btn = sc.nextInt();
+					
+					int moveTime = Math.abs(btn - O.idx);
+					
+					O.idx = btn;
+					
+					// 전에 O가 버튼을 누른 경우
+					O.time = O.time + moveTime + 1;
+					
+					// 전에 B가 버튼을 누른 경우
+					time = Math.max(moveTime, time) + 1;
+					
+					time = Math.max(time, O.time);
+					
+					// 버튼 누른 후 시간 맞춤
+					O.time = time;
+					
+
+				}
+				if(s.equals("B")){
+					
+					int btn = sc.nextInt();
+					
+					int moveTime = Math.abs(btn - B.idx);
+					
+					B.idx = btn;
+					
+					// 전에 B가 버튼을 누른 경우
+					B.time = B.time + moveTime +1;
+					
+					// 전에 O가 버튼을 누른 경우
+					time = Math.max(moveTime, time) + 1;
+					
+					time = Math.max(time, B.time);
+					
+					// 버튼 누른 후 시간 맞춤
+					B.time = time;
+
+				}
+				
+			}
+			
+			System.out.print("#"+t+" ");
+			System.out.println(time);
+		}
+	}
+}
