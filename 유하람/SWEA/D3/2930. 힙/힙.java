d0f025547591a78b43f9479a5b7ac28657d4ecbb
Ryuharam <dlfnadl97@naver.com>
2024-08-16 15:24:00 +0900

    [D3] Title: 힙, Time: 1,721 ms, Memory: 120,836 KB -SsafyHub

--- 유하람/SWEA/D3/2930. 힙/힙.java
+++ 유하람/SWEA/D3/2930. 힙/힙.java
@@ -5,44 +5,102 @@ import java.util.*;
 
 class Solution
 {
-	public static void main(String args[]) throws Exception
+	static int[] heap;
+	static int heapSize = 0;
+    
+    public static void main(String args[]) throws Exception
 	{
-		
-        
 		Scanner sc = new Scanner(System.in);
 		int T;
 		T=sc.nextInt();
 
 		for(int t = 1; t <= T; t++)
 		{
-            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-            
-            System.out.print("#"+t);
-            
-            // 연산 수
+            System.out.print("#"+t+" ");
+			
+			// 연산의 수
 			int n = sc.nextInt();
 			
+			heap = new int[n];
+			
+			heapSize = 0;
+			
 			for(int i=0 ; i<n ; i++) {
+				
 				// 연산 종류
 				int oper = sc.nextInt();
 				
 				if(oper==1) {
-					// 삽입할 숫자
-					int num = sc.nextInt();					
-					pq.add(num);
+					// 삽입
+					int num = sc.nextInt();
+					
+					add(num);
 					
 				}else {
-					if(pq.isEmpty()) {
-						System.out.print(" "+-1);
+					// 삭제
+					if(heap[1]==0) {
+						System.out.print(-1);
+						System.out.print(" ");
 					}else {
-						int tmp = pq.poll();
-						System.out.print(" "+tmp);
+						System.out.print(pop()+" ");
 					}
-
-				}
+					
+				}	
 			}
-			
 			System.out.println();
 		}
+	}
+    
+    static void swap(int i, int j) {
+		
+		int tmp = heap[i];
+		heap[i] = heap[j];
+		heap[j] = tmp;
+		
+	}
+	
+	static void add(int data) {
+		heap[++heapSize] = data;
+		
+		int p = heapSize/2;
+		int ch = heapSize;
+		
+		while(p >= 1 && heap[p] < heap[ch]) {
+			swap(p,ch);
+			
+			ch = p;
+			p = ch/2;
+		}
+		
+	}
+	
+	static int pop() {
+		
+		int popItem = heap[1];
+		
+		heap[1] = heap[heapSize];
+		heap[heapSize] = 0;
+		heapSize--;
+		
+		int p =1;
+		int ch = 2*p;
+		
+        if(ch+1 <= heapSize && heap[ch] < heap[ch+1]) {
+				ch = ch+1;
+			}
+		
+		while(ch <= heapSize && heap[p] < heap[ch]) {
+			swap(p, ch);
+			
+			p = ch;
+			ch = p*2;
+			
+			if(ch+1 <= heapSize && heap[ch] < heap[ch+1]) {
+				ch = ch+1;
+			}
+			
+		}
+
+		return popItem;
 	}
 }
