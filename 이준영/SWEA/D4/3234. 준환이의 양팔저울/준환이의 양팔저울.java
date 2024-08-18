1c85e544caea76fe768984458034777e489f1846
junyoung <jylee0619@naver.com>
2024-08-19 07:48:25 +0900

    [D4] Title: 준환이의 양팔저울, Time: 681 ms, Memory: 20,276 KB -SsafyHub

--- 이준영/SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
+++ 이준영/SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
@@ -1,44 +1,57 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int count=0;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		for (int tc=1;tc<=T;tc++) {
-			count=0;
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int[] arr = new int[N];
-			boolean[] check = new boolean[N];
-			st = new StringTokenizer(br.readLine());
-			for (int i=0;i<N;i++) {
-				arr[i]=Integer.parseInt(st.nextToken());
-			}
-			btk(0,0,0,check,arr,N);
-			System.out.printf("#%d %d\n",tc,count);
-		}
-		
-	}
-	static void btk(int left_sum,int right_sum,int c,boolean[] check,int[] arr,int N) {
-		if (c==N) {
-			count+=1;
-			return;
-		} else {
-			for (int i=0;i<N;i++) {
-				if (!check[i]) {
-					check[i]=true;
-					btk(left_sum+arr[i],right_sum,c+1,check,arr,N);
-					if(left_sum>=right_sum+arr[i]) {
-						btk(left_sum,right_sum+arr[i],c+1,check,arr,N);
-					}
-					check[i]=false;
-				}
-			}
-			return;
-		}
-	}
+
+    static int N, cnt, sum;
+    static int[] m;
+    static int[] pow= {1,2,4,8,16,32,64,128,256,512};
+    static int[] fac= { 0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder answer = new StringBuilder();
+        int T = Integer.parseInt(in.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            N = Integer.parseInt(in.readLine());
+            m = new int[N];
+            cnt = 0;
+            sum = 0;
+
+            StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+            for (int i = 0; i < N; i++) {
+                m[i] = Integer.parseInt(st.nextToken());
+                sum += m[i];
+            }
+            Arrays.sort(m);
+            recur(0, 0, 0, 0,sum);
+            answer.append('#').append(tc + 1).append(' ').append(cnt).append('\n');
+        }
+
+        System.out.print(answer);
+        in.close();
+    }
+
+    private static void recur(int index, int left, int right, int flag,int remain) {
+        if (index == N) {
+            cnt++;
+            return;
+        }
+    	if (remain+right<=left) {
+        	cnt+=pow[N-index]*fac[N-index];
+        	return;
+        }
+
+        for (int i = N-1; i >=0; i--) {
+            if ((flag&(3<<(2*i))) == 0) { // 아직 사용되지 않은 추라면
+                recur(index + 1, left + m[i], right, flag | (1 << (2 * i)),remain-m[i]);  // 왼쪽에 놓기
+                if (left>=right+m[i]) {
+                	recur(index + 1, left, right + m[i], flag | (2 << (2 * i)),remain-m[i]);  // 오른쪽에 놓기
+                }
+            }
+        }
+    }
 }
