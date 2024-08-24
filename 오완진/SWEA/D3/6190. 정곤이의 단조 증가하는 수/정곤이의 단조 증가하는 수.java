066d2d55f073e723fceb3cd9fa85a9360b84c93c
WJ <130027416+OiPKL@users.noreply.github.com>
2024-08-24 21:30:38 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 1,168 ms, Memory: 163,524 KB -SsafyHub

--- 오완진/SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
+++ 오완진/SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -1,48 +1,62 @@
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int T = sc.nextInt();
-        
-        for (int tc = 1; tc <= T; tc++) {
-        	
-        	int N = sc.nextInt();
-        	int[] danjo = new int[N];
-        	for (int i = 0; i < N; i++) {
-        		danjo[i] = sc.nextInt();
-        	}
-        	
-        	int max = -1;
-        	for (int i = 0; i < N - 1; i++) {
-        		for (int j = i + 1; j < N; j++) {
-        			int num = isDanjo(danjo[i], danjo[j]);
-        			max = Math.max(max, num);
-        		}
-        	}
-        	
-        	System.out.println("#" + tc + " " + max);
-        }
-    }
-    	
-    static int isDanjo(int a, int b) {
-    	
-    	int c = a * b;
-    	int[] jaritsu = new int[10];
-    	
-    	int j = 0;
-    	for (int i = 1000000000; i > 0; i /= 10) {
-    		jaritsu[j++] = c / i;
-    		c %= i;
-    	}
-    	
-    	for (int i = 0; i < jaritsu.length - 1; i++) {
-    		if (jaritsu[i] > jaritsu[i+1]) {
-    			return -1;
-    		}
-    	}
-    	return a * b;
-    }
-    
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int TC = sc.nextInt();
+		for (int tc = 1; tc <= TC; tc++) {
+			
+			int N = sc.nextInt();
+			int[] danjo = new int[N];
+			for (int n = 0; n < N; n++)
+				danjo[n] = sc.nextInt();
+			
+			long maxNum = -1;
+			for (int n = 0; n < N; n++) {
+				for (int m = n + 1; m < N; m++) {
+					long num = danjo[n] * danjo[m];
+					if (isDanjo(num)) 
+						maxNum = Math.max(maxNum, num);
+				}
+			}
+				
+			System.out.println("#" + tc + " " + maxNum);
+		}
+		
+	}
+	
+//	static boolean isDanjo(long num) {
+//		
+//		long tmp1 = num%10;
+//		num /= 10;
+//		
+//		while (num > 1) {
+//			long tmp2 = num % 10;
+//			if (tmp1 < tmp2) return false;
+//			tmp1 = tmp2;
+//			num /= 10;
+//		}
+//		return true;
+//		
+//	}
+	
+	static boolean isDanjo(long num) {
+		
+		String str = num + "";
+		char[] charArr = str.toCharArray();
+		int[] intArr = new int[charArr.length];
+		
+		for (int n = 0; n < charArr.length; n++)
+			intArr[n] = charArr[n] - '0';
+		
+		for (int n = 0; n < charArr.length - 1; n++)
+			if (intArr[n] > intArr[n+1]) return false;
+		
+		return true;
+		
+	}
+	
 }
