fb5f593a85b093420b7fd7f6173ab300235276a7
WJ <130027416+OiPKL@users.noreply.github.com>
2024-08-24 21:43:16 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 156 ms, Memory: 20,260 KB -SsafyHub

--- 오완진/SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ 오완진/SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -1,30 +1,32 @@
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int T = sc.nextInt();
-        sc.nextLine();
-        
-        for (int tc = 1; tc <= T; tc++) {
-        	
-        	char[] tmpArr = sc.nextLine().toCharArray();
-        	int N = tmpArr.length;
-        	int[] memory = new int[N];
-        	for (int n = 0; n < N; n++)
-        		memory[n] = (int)(tmpArr[n] - '0');
-        	int cnt = 1;
-        	
-        	// 뒤에서부터 0 - 1 - 0 바뀌는 순간 카운트
-        	// 맨 앞이 0이면 카운트--
-        	for (int n = N - 1; n > 0; n--) {
-        		if (memory[n] != memory[n-1]) cnt++;
-        	}
-        	if (memory[0] == 0) cnt--;
-        	
-        	System.out.println("#" + tc + " " + cnt);
-        }
-    }
-    
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int TC = sc.nextInt();
+		sc.nextLine();
+		for (int tc = 1; tc <= TC; tc++) {
+			
+			int cnt = 0;
+			String str = sc.nextLine();
+			char[] charArr = str.toCharArray();
+			boolean[] boolArr = new boolean[charArr.length];
+			
+			for (int c = 0; c < charArr.length; c++)
+				if (charArr[c] == '1') boolArr[c] = true;
+			
+			boolean tmp = false;
+			for (int b = 0; b < boolArr.length; b++) {
+				if (boolArr[b] != tmp) {
+					tmp = !tmp;
+					cnt++;
+				}
+			}
+			
+			System.out.println("#" + tc + " " + cnt);
+		}	
+	}
 }
