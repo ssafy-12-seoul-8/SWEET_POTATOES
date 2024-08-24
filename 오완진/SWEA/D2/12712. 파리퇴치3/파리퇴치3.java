591b7b61fdae67047b3dae535ca090ee42dfcc47
WJ <130027416+OiPKL@users.noreply.github.com>
2024-08-24 22:06:25 +0900

    [D2] Title: 파리퇴치3, Time: 173 ms, Memory: 25,376 KB -SsafyHub

--- 오완진/SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
+++ 오완진/SWEA/D2/12712. 파리퇴치3/파리퇴치3.java
@@ -1,59 +1,55 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int TC = sc.nextInt();
+	
+	static int[] dr1 = new int[] {0, 1, 0, -1};
+	static int[] dc1 = new int[] {1, 0, -1, 0};
+	static int[] dr2 = new int[] {1, 1, -1, -1};
+	static int[] dc2 = new int[] {-1, 1, 1, -1};
 
-        for (int tc = 1; tc <= TC; tc++) {
-
-        	int N = sc.nextInt();
-        	int M = sc.nextInt();
-        	int[][] pari = new int[N][N];
-        	for (int r = 0; r < N; r++) {
-        		for (int c = 0; c < N; c++) {
-        			pari[r][c] = sc.nextInt();
-        		}
-        	}
-        	
-        	List<Integer> killZone = new ArrayList<>();
-        	int kill = 0;
-        	int maxKill = 0;
-        	for (int r = 0; r < N; r++) {
-        		for (int c = 0; c < N; c++) {
-        			// + 방향
-        			killZone = new ArrayList<>();
-        			kill = 0;
-        			killZone.add(pari[r][c]);
-        			for (int m = 1; m < M; m++) {
-        				if (r+m < N) killZone.add(pari[r+m][c]);
-        				if (c+m < N) killZone.add(pari[r][c+m]);
-        				if (r-m >= 0) killZone.add(pari[r-m][c]);
-        				if (c-m >= 0) killZone.add(pari[r][c-m]);
-        			}
-        			for (int death : killZone)
-        				kill += death;
-        			maxKill = Math.max(maxKill, kill);
-        			// X 방향
-        			killZone = new ArrayList<>();
-        			kill = 0;
-        			killZone.add(pari[r][c]);
-        			for (int m = 1; m < M; m++) {
-        				if (r+m < N && c+m < N) killZone.add(pari[r+m][c+m]);
-        				if (r+m < N && c-m >= 0) killZone.add(pari[r+m][c-m]);
-        				if (r-m >= 0 && c+m < N) killZone.add(pari[r-m][c+m]);
-        				if (r-m >= 0 && c-m >= 0) killZone.add(pari[r-m][c-m]);
-        			}
-        			for (int death : killZone)
-        				kill += death;
-        			maxKill = Math.max(maxKill, kill);
-        		}
-        	}
-        	
-            System.out.println("#" + tc + " " + maxKill);
-        }
-        
-    }
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int TC = sc.nextInt();
+		for (int tc = 1; tc <= TC; tc++) {
+			
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			int[][] pari = new int[N][N];
+			for (int r = 0; r < N; r++)
+				for (int c = 0; c < N; c++)
+					pari[r][c] = sc.nextInt();
+			
+			int maxCnt = 0;
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					int cnt = 0;
+					cnt += pari[r][c];
+					for (int d = 0; d < 4; d++) {
+						for (int m = 1; m < M; m++) {
+							int rCheck = r + m * dr1[d];
+							int cCheck = c + m * dc1[d];
+							if (0 <= rCheck && rCheck < N && 0 <= cCheck && cCheck < N)
+								cnt += pari[rCheck][cCheck];
+						}
+					}
+					maxCnt = Math.max(maxCnt, cnt);
+					cnt = 0;
+					cnt += pari[r][c];
+					for (int d = 0; d < 4; d++) {
+						for (int m = 1; m < M; m++) {
+							int rCheck = r + m * dr2[d];
+							int cCheck = c + m * dc2[d];
+							if (0 <= rCheck && rCheck < N && 0 <= cCheck && cCheck < N)
+								cnt += pari[rCheck][cCheck];
+						}
+					}
+					maxCnt = Math.max(maxCnt, cnt);
+				}
+			}
+			
+			System.out.println("#" + tc + " " + maxCnt);
+		}	
+	}
 }
