4fa071ab93431fd4f4c189ae4b6ba9c80910d68f
junyoung <jylee0619@naver.com>
2024-08-27 11:46:29 +0900

    [D3] Title: 햄버거 다이어트, Time: 189 ms, Memory: 20,648 KB -SsafyHub

--- 이준영/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ 이준영/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,37 +3,40 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int max_k=0;
+	static int L;
+	static int N;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(st.nextToken());
 		for (int tc=1;tc<=T;tc++) {
-			int max_k=0;
+			max_k=0;
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int L = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
 			int[][] arr = new int[N][2];
 			for(int i=0;i<N;i++) {
 				st = new StringTokenizer(br.readLine());
 				arr[i][0] = Integer.parseInt(st.nextToken());
 				arr[i][1] = Integer.parseInt(st.nextToken());
 			}
-			for(int i=0;i<(1<<N);i++) {
-				int score=0;
-				int cal=0;
-				for(int j=0;j<N;j++) {
-					if((i&(1<<j))>0) {
-						score+=arr[j][0];
-						cal+=arr[j][1];
-					}
-				}
-				if (cal<=L && max_k<score)
-					max_k=score;
-			}
+			btk(0,0,0,arr);
 			sb.append("#").append(tc).append(" ").append(max_k).append("\n");
 			
 		}
 		System.out.println(sb);
+	}
+	public static void btk(int index,int score,int cal,int[][] arr) {
+		if(cal>L)
+			return;
+		if(index==N) {
+			max_k=Math.max(max_k, score);
+			return;
+		}
+		btk(index+1,score+arr[index][0],cal+arr[index][1],arr);
+		btk(index+1,score,cal,arr);
+		
 	}
 }
