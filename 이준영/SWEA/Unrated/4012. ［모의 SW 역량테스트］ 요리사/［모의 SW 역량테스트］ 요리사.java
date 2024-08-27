27012bca5224261ba6db35a589a56468238d3ac6
junyoung <jylee0619@naver.com>
2024-08-27 11:59:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 219 ms, Memory: 25,616 KB -SsafyHub

--- 이준영/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ 이준영/SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Solution {
+	static int mini = 10000000;
+	static int N;
+	static int[][] arr ;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,41 +14,45 @@ public class Solution {
 		int T = Integer.parseInt(st.nextToken());
 		for(int tc=1;tc<=T;tc++) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int mini = 10000000;
-			int[][] arr = new int[N][N];
+			N = Integer.parseInt(st.nextToken());
+			mini = 10000000;
+			arr = new int[N][N];
 			for(int i=0;i<N;i++) {
 				st= new StringTokenizer(br.readLine());
 				for(int j=0;j<N;j++) {
 					arr[i][j]=Integer.parseInt(st.nextToken());
 				}
 			}
-			for(int i=0;i<(1<<N);i++) {
-				int sum=0;
-				int count=0;
-				ArrayList<Integer> arr1 = new ArrayList<>();
-				ArrayList<Integer> arr2 = new ArrayList<>();
-				for(int j=0;j<N;j++) {
-					if((i&(1<<j))==0) {
-						arr1.add(j);
-						count+=1;
-					} else {
-						arr2.add(j);
-					}
-				}
-				if(count!=N/2)
-					continue;
-				for(int k=0;k<N/2;k++) {
-					for(int j=0;j<N/2;j++) {
-						sum=sum+arr[arr1.get(k)][arr1.get(j)]-arr[arr2.get(k)][arr2.get(j)];
-					}
-				}
-				sum=Math.abs(sum);
-				if(mini>sum)
-					mini=sum;	
-			}
+			ArrayList<Integer> arr1 = new ArrayList<>();
+			ArrayList<Integer> arr2 = new ArrayList<>();
+			btk(0,0,arr1,arr2);
 			sb.append("#").append(tc).append(" ").append(mini).append("\n");
 		}
 		System.out.println(sb);
+	}
+	static void btk(int count1,int count2,ArrayList<Integer> arr1,ArrayList<Integer> arr2) {
+		if (count1>N/2|count2>N/2) {
+			return;
+		}
+		if (count1+count2==N)
+			check(arr1,arr2);
+		arr1.add(count1+count2);
+		btk(count1+1,count2,arr1,arr2);
+		arr1.remove(count1);
+		arr2.add(count1+count2);
+		btk(count1,count2+1,arr1,arr2);
+		arr2.remove(count2);
+		
+	}
+	static void check(ArrayList<Integer> arr1,ArrayList<Integer> arr2) {
+		int sum=0;
+		for(int k=0;k<N/2;k++) {
+			for(int j=0;j<N/2;j++) {
+				sum=sum+arr[arr1.get(k)][arr1.get(j)]-arr[arr2.get(k)][arr2.get(j)];
+			}
+		}
+		sum=Math.abs(sum);
+		if(mini>sum)
+			mini=sum;
 	}
 }
