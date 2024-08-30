c6027118bbac2a1b38716bbef873588878a546aa
Ryuharam <dlfnadl97@naver.com>
2024-08-30 23:17:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 73.1 MB -SsafyHub

---
+++ 유하람/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        
+        while(a!=b){
+            a = (a+1)/2;
+            b = (b+1)/2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
