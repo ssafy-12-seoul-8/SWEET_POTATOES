4ab748e27a85a1b57437706df556d1c781cb6e6c
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-01 16:44:24 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,091 ms, Memory: 23,676 KB -SsafyHub

--- 오완진/SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ 오완진/SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,69 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
 	
-	static boolean[] cards;		// idx: 1 ~ 18	- 인영이의 카드덱 결정에 사용
-	static boolean[] iinPicked;	// idx: 0 ~ 8	- 백트래킹 중복 선택 방지
-	static int[] kyuOrder;		// idx: 0 ~ 8	- 규영이의 카드순서
-	static int[] iinCard;		// idx: 0 ~ 8	- 인영이의 카드덱 (순서는 미정)
-	static int[] iinOrder;		// idx: 0 ~ 8	- 인영이의 카드순서
-	static int kyuWin;
-	static int iinWin;
+	static int[] kyuOrder;
+	static int[] iinCards;
+	static boolean[] visited;
+	static List<Integer> iinOrder;
+	static int kyuWin, iinWin;
 	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
+
 		int TC = sc.nextInt();
 		for (int tc = 1; tc <= TC; tc++) {
 			
-			cards = new boolean[19];
-			iinPicked = new boolean[9];
 			kyuOrder = new int[9];
-			iinCard = new int[9];
-			iinOrder = new int[9];
+			iinCards = new int[9];
+			visited = new boolean[9];
+			iinOrder = new ArrayList<>();
 			kyuWin = 0;
 			iinWin = 0;
+			boolean[] cardCheck = new boolean[19];
 			
 			for (int i = 0; i < 9; i++) {
 				int card = sc.nextInt();
 				kyuOrder[i] = card;
-				cards[card] = true;
+				cardCheck[card] = true;
 			}
 			
 			int iinIdx = 0;
-			for (int i = 1; i < 19; i++) {
-				if (!cards[i]) iinCard[iinIdx++] = i;
-			}
+			for (int i = 1; i < 19; i++)
+				if (!cardCheck[i])
+					iinCards[iinIdx++] = i;
 			
-			play (0, 0, 0);
+			btk(0, 0);
 			
 			System.out.println("#" + tc + " " + kyuWin + " " + iinWin);
-			
 		}
 		
 	}
 	
-	static void play(int iinIdx, int kyuScore, int iinScore) {
-		
-		if (iinIdx == 9) {
+	static void btk(int kyuScore, int iinScore) {
+		if (iinOrder.size() == 9) {
 			if (kyuScore > iinScore) kyuWin++;
 			else					 iinWin++;
-
+			
 			return;
 		}
 		
 		for (int i = 0; i < 9; i++) {
-			if (!iinPicked[i]) {
-				iinPicked[i] = true;
-				if (kyuOrder[i] > iinCard[iinIdx])
-					play(iinIdx + 1, kyuScore + kyuOrder[i] + iinCard[iinIdx], iinScore);
+			if (!visited[i]) {
+				visited[i] = true;
+				iinOrder.add(iinCards[i]);
+				if (kyuOrder[iinOrder.size() - 1] > iinOrder.get(iinOrder.size() - 1))
+					btk(kyuScore + kyuOrder[iinOrder.size() - 1] + iinOrder.get(iinOrder.size() - 1), iinScore);
 				else
-					play(iinIdx + 1, kyuScore, iinScore + kyuOrder[i] + iinCard[iinIdx]);
-				iinPicked[i] = false;
+					btk(kyuScore, iinScore + kyuOrder[iinOrder.size() - 1] + iinOrder.get(iinOrder.size() - 1));
+				iinOrder.remove(iinOrder.size() - 1);
+				visited[i] = false;
 			}
 		}
-		
 	}
 	
 }
