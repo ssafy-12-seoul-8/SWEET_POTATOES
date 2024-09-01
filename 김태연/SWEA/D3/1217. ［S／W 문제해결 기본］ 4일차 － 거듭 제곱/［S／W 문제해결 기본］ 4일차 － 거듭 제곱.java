329f344792068bc64ed263343a125897b803f24b
skybory <154962211+skybory@users.noreply.github.com>
2024-09-01 11:17:39 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 142 ms, Memory: 19,928 KB -SsafyHub

--- 김태연/SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ 김태연/SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,32 +1,25 @@
 import java.util.Scanner;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
+public class Solution {
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=10;
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-		
-			int A;
-			A = sc.nextInt();
-			int N,M;
-			N = sc.nextInt();
-			M = sc.nextInt();
-
+		for (int test_case = 1; test_case <= 10; test_case++) {
+			sc.nextInt();
+			
+			int N = sc.nextInt();	// 9
+			int M = sc.nextInt();	// 8
+			
+			// 9의 8제곱
 			int result = power(N,M);
-			
 			System.out.println("#" + test_case + " " + result);
-			
 			
 		}
 	}
 	
 	static int power(int N, int M) {
-		if (M==0) return 1;
-		else return N * power(N,M-1);
+		if (M==1) return N;
+		return N * power(N,M-1);
 	}
 }
+
