40d4602cf5eea0d5bf91f561e1e6e2d4a2a0caf2
Ryuharam <dlfnadl97@naver.com>
2024-09-01 11:43:34 +0900

    [Silver III] Title: N과 M (1), Time: 252 ms, Memory: 23020 KB -SsafyHub

--- 유하람/백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 유하람/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,57 +1,63 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main{
+
+	static StringBuilder sb = new StringBuilder();
 	
-	static int N;
-	static int M;
-	static int[] arr;
-	static boolean[] visit;
-	static List<int[]> list = new ArrayList<>();
+    public static void main(String[] args) throws Exception{
     
-    public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-        
-        N = sc.nextInt();
-        M = sc.nextInt();
-        
-        arr = new int[N];
-        for(int i=0 ; i<N ; i++){
-            arr[i] = i+1;
-        }
-        
-        visit = new boolean[N];
-        int[] datas = new int[M];
-        
-        NM(0,datas);
-        
-        for(int[] data : list) {
-        	for(int i : data) {
-        		System.out.print(i+" ");
-        	}
-        	System.out.println();
-        }
-        
-        
-    }
-    
-    static void NM(int idx, int[] datas) {
-    	if(idx==M) {
-    		list.add(datas.clone());
-    		return;
-    	}
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	String nm = br.readLine();
+    	StringTokenizer st = new StringTokenizer(nm);
+    	
+    	int N = Integer.parseInt(st.nextToken());
+    	
+    	int M = Integer.parseInt(st.nextToken());
+    	
+    	// 1 ~ N 의 자연수
+    	int[] arr = new int[N];
     	for(int i=0 ; i<N ; i++) {
-    		if(!visit[i]) {
-	    		visit[i] = true;
-	    		datas[idx] = arr[i];
-	    		NM(idx+1, datas);
-	    		visit[i] = false;
-    		}
+    		arr[i] = i+1;
     	}
     	
+    	boolean[] visited = new boolean[N];
+    	int[] data = new int[M];
+    	permutation(arr, N, data, M, 0, visited);
+    	
+    	String ans = sb.toString();
+    	bw.write(ans);
+    	bw.flush();
+    	bw.close();
+    	br.close();
+    	
     }
-    
+
+	private static void permutation(int[] arr,int N, int[] data, int m, int depth, boolean[] visited) {
+		if(depth==m) {
+			for(int i : data) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=0 ; i<arr.length ; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				
+				data[depth] = arr[i];
+				permutation(arr, N, data, m, depth+1, visited);
+				
+				visited[i] = false;
+			}
+		}
+	}
+
     
 }
