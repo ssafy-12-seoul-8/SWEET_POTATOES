a9e847bc6a519adf6a5c8fb5d1ddd512769563bf
Ryuharam <dlfnadl97@naver.com>
2024-09-01 12:21:46 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 16380 KB -SsafyHub

--- 유하람/백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 유하람/백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,69 +1,61 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+
+	static StringBuilder sb = new StringBuilder();
 	
-	static int n;
-	static int m;
-	static int[] arr;
-	static List<int[]> list = new ArrayList<>();
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		
-		StringTokenizer st = new StringTokenizer(s);
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		arr = new int[n];
-		
-		for(int i=0 ; i<n ; i++) {
-			arr[i] = i+1;
-		}
-		
-		int[] data = new int[m];
-		NM(data, 0, 0);
-		
-		String result = "";
-		StringBuilder sb = new StringBuilder();
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		for(int[] d : list) {
-			for(int i : d) {
+    public static void main(String[] args) throws Exception{
+    
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	String nm = br.readLine();
+    	StringTokenizer st = new StringTokenizer(nm);
+    	
+    	int N = Integer.parseInt(st.nextToken());
+    	
+    	int M = Integer.parseInt(st.nextToken());
+    	
+    	// 1 ~ N 의 자연수
+    	int[] arr = new int[N];
+    	for(int i=0 ; i<N ; i++) {
+    		arr[i] = i+1;
+    	}
+    	
+    	int[] data = new int[M];
+    	
+    	combination(arr, N, data, M, 0, 0);
+    	
+    	String ans = sb.toString();
+    	bw.write(ans);
+    	bw.flush();
+    	bw.close();
+    	br.close();
+    	
+    }
+
+	private static void combination(int[] arr, int n, int[] data, int m, int depth, int idx) {
+		if(depth==m) {
+			for(int i : data) {
 				sb.append(i).append(" ");
-				
 			}
 			sb.append("\n");
-		}
-		
-		result = sb.toString();
-		bw.write(result);
-		bw.flush();
-		bw.close();
-
-	}
-	
-	static void NM(int[] data, int idx, int start) {
-		if(idx == m) {
-			list.add(data.clone());
 			return;
 		}
 		
-		for(int i=start ; i<n ; i++) {
-			data[idx] = arr[i];
-			NM(data, idx+1, i);		
+		for(int i=idx ; i<n ; i++) {
+			data[depth] = arr[i];
+			combination(arr,n,data,m,depth+1,i);
 		}
 	}
-	
 
 
+
+
+
+    
 }
