76854294ab959cf445e22aca8466b6ca9f9ee99d
Ryuharam <dlfnadl97@naver.com>
2024-09-01 16:51:06 +0900

    [Silver II] Title: N과 M (11), Time: 316 ms, Memory: 34520 KB -SsafyHub

--- 유하람/백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 유하람/백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -2,100 +2,71 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
 
-class ArrayWrapper{
-	int[] array;
-	
-	ArrayWrapper(int[] array){
-		this.array = array;
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if(this == o)
-			return true;
-		if(o==null || getClass()!= o.getClass())
-			return false;
-		ArrayWrapper that = (ArrayWrapper) o;
-		return Arrays.equals(array, that.array);
-	}
-	
-	@Override
-	public int hashCode() {
-		return Arrays.hashCode(array);
-	}
-}
-
 public class Main{
-	
-	static Set<ArrayWrapper> set = new HashSet<>();
+
+	static StringBuilder sb = new StringBuilder();
 	
     public static void main(String[] args) throws Exception{
-    	
+    
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	StringBuilder sb = new StringBuilder();
     	
-    	String s = br.readLine();
-    	StringTokenizer st = new StringTokenizer(s);
+    	String nm = br.readLine();
+    	StringTokenizer st = new StringTokenizer(nm);
     	
     	int N = Integer.parseInt(st.nextToken());
+    	
     	int M = Integer.parseInt(st.nextToken());
     	
-    	String str = br.readLine();
-    	StringTokenizer st2 = new StringTokenizer(str);
     	
+    	// N개의 자연수
     	int[] arr = new int[N];
-    	
+    	String a = br.readLine();
+    	StringTokenizer st2 = new StringTokenizer(a);
     	for(int i=0 ; i<N ; i++) {
     		arr[i] = Integer.parseInt(st2.nextToken());
     	}
     	
     	Arrays.sort(arr);
     	
-    	List<int[]> list = new ArrayList<>();
-    	int[] data = new int[M];    	
-    	NM(N, M, arr, 0, data, list);
+    	int[] data = new int[M];
     	
-    	// 출력
-    	for(int[] d : list) {
-    		for(int i : d) {
-    			sb.append(i).append(" ");
-    		}
-    		sb.append("\n");
-    	}
+    	permutation(arr,N,data,M,0);
+    	
     	
     	String ans = sb.toString();
     	bw.write(ans);
     	bw.flush();
     	bw.close();
+    	br.close();
     	
     }
+
+	private static void permutation(int[] arr, int n, int[] data, int m, int depth) {
+		if(depth==m) {
+			for(int i : data) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int prev = -1;
+		
+		for(int i=0 ; i<n ; i++) {
+			if(arr[i]!=prev) {
+				data[depth] = arr[i];
+				prev = arr[i];
+				permutation(arr,n,data,m,depth+1);
+			}
+		}
+		
+	}
+
+
+
     
-    static void NM(int N, int M , int[]arr, int depth, int[] data, List<int[]> list) {
-    	
-    	if(depth == M) {
-    		int[] tmp = data.clone();
-    		if(!set.contains(new ArrayWrapper(tmp))) {
-    			set.add(new ArrayWrapper(tmp));
-    			list.add(tmp);
-    		}
-    		return;
-    	}
-    	
-    	for(int i=0 ; i<N ; i++) {
-    		data[depth] = arr[i];
-    		NM(N, M, arr, depth+1, data, list);
-    		
-    	}
-    	
-    	
-    }
-  
 }
