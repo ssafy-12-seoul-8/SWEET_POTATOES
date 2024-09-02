9c8d2c2b5d78f493e5dc97f135c2aeaa62d1a5bc
junyoung0619 <jylee0619@naver.com>
2024-09-02 17:53:11 +0900

    create B형 복기

---
+++ 이준영/B형 복기/src/UserSolution.java
@@ -0,0 +1,12 @@
+
+public class UserSolution {
+	static void init(int N, int[][] mPlane) {
+		
+	}
+	static int count_stars(int[][] stars) {
+		
+	}
+	static int find_loaction(int y, int x) {
+		
+	}
+}
