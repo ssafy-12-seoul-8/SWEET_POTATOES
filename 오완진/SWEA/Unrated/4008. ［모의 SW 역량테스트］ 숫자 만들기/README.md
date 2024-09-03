c8f42959a9ba7409863ff5e0e73ea480d9284bbd
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-03 12:45:23 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 261 ms, Memory: 48,820 KB -SsafyHub

---
+++ 오완진/SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 숫자 만들기 - 4008 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWIeRZV6kBUDFAVH) 
+
+### 성능 요약
+
+메모리: 48,820 KB, 시간: 261 ms, 코드길이: 1,477 Bytes
+
+### 제출 일자
+
+2024-09-03 12:45
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ 오완진/SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+	
+	static int N;
+	static List<List<Integer>> orderList;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int TC = sc.nextInt();
+		for (int tc = 1; tc <= TC; tc++) {
+			
+			N = sc.nextInt();
+			int[] nums = new int[N];
+			int[] ops = new int[4];
+			for (int n = 0; n < 4; n++)
+				ops[n] = sc.nextInt();
+			for (int n = 0; n < N; n++)
+				nums[n] = sc.nextInt();
+			
+			long max = Long.MIN_VALUE;
+			long min = Long.MAX_VALUE;
+			
+			orderList = new ArrayList<>();
+			makeOrderList(ops, new ArrayList<>());
+			
+			for (List<Integer> order : orderList) {
+				long ans = nums[0];
+				for (int i = 1; i < N; i++) {
+					int num = nums[i];
+					int op = order.get(i - 1);
+					
+					if (op == 0)		ans += num;
+					if (op == 1)		ans -= num;
+					if (op == 2)		ans *= num;
+					if (op == 3) {
+						if (ans > 0)	ans /= num;
+						else			ans = -(-ans)/num;
+					
+					}
+				}
+				max = Math.max(max, ans);
+				min = Math.min(min, ans);
+			}
+			
+			System.out.println("#" + tc + " " + (max - min));
+		}
+	}
+	
+	static void makeOrderList(int[] ops, List<Integer> order) {
+		
+		if (order.size() == N - 1) {
+			orderList.add(new ArrayList<>(order));
+			return;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			if (ops[i] > 0) {
+				ops[i]--;
+				order.add(i);
+				makeOrderList(ops, order);
+				order.remove(order.size() - 1);
+				ops[i]++;
+			}
+		}
+		
+	}
+}
