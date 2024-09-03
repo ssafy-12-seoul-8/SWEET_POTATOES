e6e2ab68caceff7acc7fbcf0dc04d25c4d35bd27
Ryuharam <dlfnadl97@naver.com>
2024-09-04 00:04:45 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 190 ms, Memory: 24,064 KB -SsafyHub

--- 유하람/SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ 유하람/SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,56 +1,60 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Solution {
 	
+	static int answer;
+	static int[] plan;
+	static int tMonth;
+
 	public static void main(String[] args) throws Exception {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner sc = new Scanner(System.in);			
 		
-		String t = br.readLine();
-		int T = Integer.parseInt(t);
+		int T = sc.nextInt();
 		
 		for(int tc=1 ; tc<=T ; tc++) {
 			
-			String s = br.readLine();
-			StringTokenizer st1 = new StringTokenizer(s);
+			int day = sc.nextInt();
+			int month = sc.nextInt();
+			tMonth = sc.nextInt();
+			int year = sc.nextInt();
 			
-			int day = Integer.parseInt(st1.nextToken());
-			int month = Integer.parseInt(st1.nextToken());
-			int tMonth = Integer.parseInt(st1.nextToken());
-			int year = Integer.parseInt(st1.nextToken());
+			plan = new int[13];
 			
-			int[] plan = new int[13];
-			
-			String p = br.readLine();
-			StringTokenizer st2 = new StringTokenizer(p);
-			
-			// 일일권보다 한달권이 싼 경우 한달권 선택
-			// 각 달에 그 달까지의 최소 비용 저장
 			for(int i=1 ; i<13 ; i++) {
-				int num = Integer.parseInt(st2.nextToken());
+				int num = sc.nextInt();
 				
-				if(num*day < month) {	
-					plan[i] = plan[i-1] + num*day;
+				if(num*day>month) {
+					plan[i] = month;
 				}else {
-					plan[i] = plan[i-1] + month;
-				}
-				
-
-				// 3개월 이후부터는 3개월권도 생각
-				if(i>=3) {
-					if(plan[i] > plan[i-3] + tMonth) {
-						plan[i] =  plan[i-3] + tMonth;
-					}
+					plan[i] = num*day;
 				}
 			}
 			
-			int answer = Math.min(plan[12], year);
+			answer =Integer.MAX_VALUE;
+			
+			btk(0, 0);
+			
+			answer = Math.min(answer, year);
 			
 			System.out.println("#"+tc+" "+answer);
 			
 		}
+		
+		
+	}
+
+	private static void btk(int m, int result) {
+		if(m>12) {
+			answer = Math.min(answer, result);
+			return;
+		}
+		
+		// m월에 3개월권 선택한다면
+		btk(m+3, result+tMonth);
+		
+		// 선택 안한다면
+		btk(m+1, result+plan[m]);
 		
 	}
 
