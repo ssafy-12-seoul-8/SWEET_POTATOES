5cc9c1e4e24301417157d3fce9f5932d01715565
junyoung <jylee0619@naver.com>
2024-09-03 11:30:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 숫자 만들기, Time: 149 ms, Memory: 21,232 KB -SsafyHub

---
+++ 이준영/SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 숫자 만들기 - 4008 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWIeRZV6kBUDFAVH) 
+
+### 성능 요약
+
+메모리: 21,232 KB, 시간: 149 ms, 코드길이: 1,670 Bytes
+
+### 제출 일자
+
+2024-09-03 11:30
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ 이준영/SWEA/Unrated/4008. ［모의 SW 역량테스트］ 숫자 만들기/［모의 SW 역량테스트］ 숫자 만들기.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class Solution {
+	static int[] count;
+	static int max_result;
+	static int min_result= 1000000001;
+	static int N=0;
+	static int[] arr;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(st.nextToken());
+		
+		for(int tc=1;tc<=T;tc++) {
+			st= new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			arr = new int[N];
+			count = new int[4];
+			max_result = -1000000001;
+			min_result= 1000000001;
+			st= new StringTokenizer(br.readLine());
+			
+			for(int i=0;i<4;i++) {
+				count[i]=Integer.parseInt(st.nextToken());
+			}
+			
+	 		st= new StringTokenizer(br.readLine());
+			for(int i=0;i<N;i++) {
+				arr[i]=Integer.parseInt(st.nextToken());
+			}
+			btk(0,arr[0]);
+			sb.append("#").append(tc).append(" ").append(max_result-min_result).append("\n");
+		}
+		System.out.println(sb);
+		
+	}
+	public static void btk(int cur,int result) {
+		if (cur==N-1) {
+			max_result=Math.max(max_result,result);
+			min_result=Math.min(min_result,result);
+			return;
+		}
+		for(int i=0;i<4;i++) {
+			if(count[i]!=0) {
+				count[i]-=1;
+				switch (i) {
+					case 0: 
+						btk(cur+1,result+arr[cur+1]);
+						break;
+					case 1: 
+						btk(cur+1,result-arr[cur+1]);
+						break;
+					case 2: 
+						btk(cur+1,result*arr[cur+1]);
+						break;
+					case 3: 
+						btk(cur+1,result/arr[cur+1]);
+						break;
+				}
+				count[i]+=1;
+			}
+		}
+		return;
+	}
+}
