4f0d573e5a469e896aba759444f62e334116b681
junyoung <jylee0619@naver.com>
2024-09-04 08:07:36 +0900

    Update 서울 지하철 2호선.java

--- 이준영/백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
+++ 이준영/백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
@@ -37,39 +37,39 @@ public class Main {
 		
 		Queue<Integer> queue = new LinkedList<>();
 		
-		for(int i=1;i<=N;i++) {
-			if(count1[i]==1) {
+		for(int i=1;i<=N;i++) {				// 리프에 있는 역으로부터 bfs를 할거라서 이들을 가져옴
+			if(count1[i]==1) {				// count1은 실질적으로 visit하지 않은 역 중 방문할 수 있는 역
 				queue.add(i);
 				count1[i]=0;
 			}
 		}
 		
 		while(!queue.isEmpty()) {
-			int a = queue.poll();
+			int a = queue.poll();			// queue에는 항상 방문할 수 있는 역이 하나인 역만 들어감
 			if(!visited[a]) {
 				visited[a] = true;
 				for(int i:arr[a]) {
 					if(!visited[i]) {
 						count1[i]-=1;
-						if(count1[i]==1) {
+						if(count1[i]==1) {  // 그 역이 이제 방문할 수 있는 역이 하나가 되면 queue에 넣음
 							queue.add(i);
 						}
-						break;
+						break;  			// 어차피 연결된 방문가능한 역이 하나이므로 break
 					}
 				}
 			}
 		}
 		
 		for(int i=1;i<=N;i++) {
-			if(!visited[i] && count2[i]>=3) {
+			if(!visited[i] && count2[i]>=3) { // 순환선에서 count2가 3이상인 것은 지선과 연결된 것
 				queue.add(i);
 			}
 		}
 		
-		while(!queue.isEmpty()) {
+		while(!queue.isEmpty()) {				// 지선과 연결된 순환선의 역에서 bfs
 			int a = queue.poll();
 			for(int i:arr[a]) {
-				if(visited[i]) {
+				if(visited[i]) {				// 아직 방문하지 않은 노드가 visited 값이 true가 됨
 					visited[i] = false;
 					depth[i] = depth[a]+1;
 					queue.add(i);
