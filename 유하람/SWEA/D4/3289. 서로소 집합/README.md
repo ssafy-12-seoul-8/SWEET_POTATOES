c46ede79e9049243c1c9269ee2fe0bc48e5c23e9
Ryuharam <dlfnadl97@naver.com>
2024-09-04 12:29:51 +0900

    [D4] Title: 서로소 집합, Time: 2,441 ms, Memory: 130,192 KB -SsafyHub

---
+++ 유하람/SWEA/D4/3289. 서로소 집합/README.md
@@ -0,0 +1,15 @@
+# [D4] 서로소 집합 - 3289 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBJKA6qr2oDFAWr) 
+
+### 성능 요약
+
+메모리: 130,192 KB, 시간: 2,441 ms, 코드길이: 940 Bytes
+
+### 제출 일자
+
+2024-09-04 12:29
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ 유하람/SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Solution {
+	
+	static int[] p;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int tc=1 ; tc<=T ; tc++) {
+			
+			System.out.print("#"+tc+" ");
+			
+			int V = sc.nextInt();
+			int E = sc.nextInt();
+			
+			p = new int[V+1];
+			
+			for(int i=1 ; i<=V ; i++) {
+				p[i] = i;
+			}
+			
+			for(int i=0 ; i<E ; i++) {
+				
+				int oper = sc.nextInt();
+				int A = sc.nextInt();
+				int B = sc.nextInt();
+				
+				int pa = findSet(A);
+				int pb = findSet(B);
+				
+				if(oper==0) {
+					p[pa] = pb;
+				}else {
+					
+					// 갱신
+					p[A] = p[pa];
+					p[B] = p[pb];
+					
+					if(p[A]==p[B]) {
+						System.out.print(1);
+					}else {
+						System.out.print(0);
+					}
+				}
+				
+			}
+			
+			System.out.println();
+		}
+		
+		
+	}
+
+
+	private static int findSet(int b) {
+		if(b != p[b]) {
+			p[b] = findSet(p[b]);
+		}
+		return p[b];
+	}
+
+
+}
