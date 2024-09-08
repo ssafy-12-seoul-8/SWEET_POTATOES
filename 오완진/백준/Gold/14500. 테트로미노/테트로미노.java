0e4e9bacacf51e28438d91d3dece7ae5f30c9db9
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-09 08:04:24 +0900

    [Gold IV] Title: 테트로미노, Time: 1228 ms, Memory: 170736 KB -SsafyHub

--- 오완진/백준/Gold/14500. 테트로미노/테트로미노.java
+++ 오완진/백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,257 +1,131 @@
-import java.util.ArrayList;
-
-import java.util.Arrays;
-
-import java.util.List;
-
 import java.util.Scanner;
 
 public class Main {
 
-		static List<List<Integer>> pickList = new ArrayList<>();
-
-	public static void main(String[] args) {
-
-		
-
-		Scanner sc = new Scanner(System.in);
-
-		
-
-		int N = sc.nextInt();
-
-		int M = sc.nextInt();
-
-		int[][] map = new int[N][M];
-
-		
-
-		for (int n = 0; n < N; n++)
-
-			for (int m = 0; m < M; m++)
-
-				map[n][m] = sc.nextInt();
-
-		
-
-		int maxSum = Integer.MIN_VALUE;
-
-		
-
-		combination(new ArrayList<>(), 0);			// 6C2
-
-		// 4 x 1
-
-		for (int n = 0; n < N; n++) {
-
-			for (int m = 0; m <= M - 4; m++) {
-
-				int sum = 0;
-
-				sum += map[n][m];
-
-				sum += map[n][m+1];
-
-				sum += map[n][m+2];
-
-				sum += map[n][m+3];
-
-				maxSum = Math.max(maxSum, sum);
-
-			}
-
-		}
-
-		// 1 x 4
-
-		for (int n = 0; n <= N - 4; n++) {
-
-			for (int m = 0; m < M; m++) {
-
-				int sum = 0;
-
-				sum += map[n][m];
-
-				sum += map[n+1][m];
-
-				sum += map[n+2][m];
-
-				sum += map[n+3][m];
-
-				maxSum = Math.max(maxSum, sum);
-
-			}
-
-		}
-
-		// 2 x 2
-
-		for (int n = 0; n <= N -2; n++) {
-
-			for (int m = 0; m <= M - 2; m++) {
-
-				int sum = 0;
-
-				sum += map[n][m];
-
-				sum += map[n][m+1];
-
-				sum += map[n+1][m];
-
-				sum += map[n+1][m+1];
-
-				maxSum = Math.max(maxSum, sum);
-
-			}
-
-		}
-
-		// 3 x 2 -> 4개 pick
-
-		for (int n = 0; n <= N - 2; n++) {
-
-			for (int m = 0; m <= M - 3; m++) {
-
-				int sum = 0;
-
-				int[] tet = {map[n][m], map[n][m+1], map[n][m+2], map[n+1][m], map[n+1][m+1], map[n+1][m+2]};
-
-				
-
-				int minSum = Integer.MAX_VALUE;
-
-                for (List<Integer> pick : pickList) {
-
-                	
-
-                	if (pick.get(0) == 0 && pick.get(1) == 4) continue;
-
-					if (pick.get(0) == 1 && pick.get(1) == 3) continue;
-
-					if (pick.get(0) == 1 && pick.get(1) == 4) continue;
-
-					if (pick.get(0) == 1 && pick.get(1) == 5) continue;
-
-					if (pick.get(0) == 2 && pick.get(1) == 4) continue;
-
-					
-
-                    int tmp = tet[pick.get(0)] + tet[pick.get(1)];
-
-					minSum = Math.min(minSum, tmp);			// 2개 최소값 -> 나머지 4개 최대값
-
-				}
-
-				
-
-				for (int i = 0; i < 6; i++)
-
-					sum += tet[i];
-
-				
-
-				sum -= minSum;
-
-				
-
-				maxSum = Math.max(maxSum, sum);
-
-			}
-
-		}
-
-		// 2 x 3 -> 4개 pick
-
-		for (int n = 0; n <= N - 3; n++) {
-
-			for (int m = 0; m <= M - 2; m++) {
-
-				int sum = 0;
-
-				int[] tet = {map[n][m], map[n][m+1], map[n+1][m], map[n+1][m+1], map[n+2][m], map[n+2][m+1]};
-
-				
-
-				int minSum = Integer.MAX_VALUE;
-
-                for (List<Integer> pick : pickList) {
-
-                	
-
-					if (pick.get(0) == 0 && pick.get(1) == 3) continue;
-
-					if (pick.get(0) == 1 && pick.get(1) == 2) continue;
-
-					if (pick.get(0) == 2 && pick.get(1) == 3) continue;
-
-					if (pick.get(0) == 2 && pick.get(1) == 5) continue;
-
-					if (pick.get(0) == 3 && pick.get(1) == 4) continue;
-
-					
-
-                    int tmp = tet[pick.get(0)] + tet[pick.get(1)];
-
-					minSum = Math.min(minSum, tmp);			// 2개 최소값 -> 나머지 4개 최대값
-
-				}
-
-				
-
-				for (int i = 0; i < 6; i++)
-
-					sum += tet[i];
-
-				
-
-				sum -= minSum;
-
-				
-
-				maxSum = Math.max(maxSum, sum);
-
-			}
-
-		}
-
-		
-
-		System.out.println(maxSum);
-
-	}
-
-	
-
-	// 6 combination 2 = 15
-
-	static void combination(List<Integer> pick, int start) {
-
-		
-
-		if (pick.size() == 2) {
-
-			pickList.add(new ArrayList<>(pick));
-
-			return;
-
-		}
-
-		
-
-		for (int i = start; i < 6; i++) {
-
-			pick.add(i);
-
-			combination(pick, i + 1);
-
-			pick.remove(pick.size() - 1);
-
-		}
-
-		
-
-	}
-
-	
-
+    static int[] dn = {-1, 0, 1, 0};
+    static int[] dm = {0, 1, 0, -1};
+    static int N, M, maxSum;
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        for (int n = 0; n < N; n++)
+            for (int m = 0; m < M; m++)
+                map[n][m] = sc.nextInt();
+
+        maxSum = 0;
+        // 가로2개 고정
+        for (int nNow = 0; nNow < N; nNow++) {
+            for (int mNow = 0; mNow < M - 1; mNow++) {
+                visited[nNow][mNow] = true;
+                visited[nNow][mNow + 1] = true;
+                
+                int sum = map[nNow][mNow] + map[nNow][mNow + 1];
+                btk1(nNow, mNow, 2, sum);
+                btk2(nNow, mNow, 0, 0, 2, sum, 1);
+
+                visited[nNow][mNow] = false;
+                visited[nNow][mNow + 1] = false;
+            }
+        }
+        
+        // 세로4개 고정
+        for (int nNow = 0; nNow <= N - 4; nNow++) {
+        	for (int mNow = 0; mNow < M; mNow++) {
+        		int sum = 0;
+        		for (int d = 0; d < 4; d++) {
+        			sum += map[nNow + d][mNow];
+        		}
+        		maxSum = Math.max(maxSum, sum);
+        	}
+        }
+
+        System.out.println(maxSum);
+    }
+    
+    // btk1 : 가로 2블럭 주변에서 2개 pick
+    static void btk1(int nNow, int mNow, int cnt, int sum) {
+    	if (cnt >= 4) {
+    		maxSum = Math.max(maxSum, sum);
+    		return;
+    	}
+    	
+    	for (int i = -1; i <= 1; i++) {
+    		for (int j = -1; j <= 2; j++) {
+    			
+    			if (i == -1 && j == -1) continue;		// 각 모서리
+    			if (i == -1 && j == 2) continue;
+    			if (i == 1 && j == -1) continue;
+    			if (i == 1 && j == 2) continue;
+    			
+    			int nNext = nNow + i;
+    			int mNext = mNow + j;
+    			
+    			if (nNext < 0 || N <= nNext) continue;
+    			if (mNext < 0 || M <= mNext) continue;
+    			if (visited[nNext][mNext]) continue;
+    			
+    			visited[nNext][mNext] = true;
+    			btk1(nNow, mNow, cnt + 1, sum + map[nNext][mNext]);
+    			visited[nNext][mNext] = false;
+    		}//j
+    	}//i
+    }
+    
+    // btk2 : 가로 2블럭 주변에서 1개 pick - 이 1개 블럭 주변에서 1개 pick
+    static void btk2(int nNow, int mNow, int nNext, int mNext, int cnt, int sum, int level) {
+    	if (cnt >= 4) {
+    		maxSum = Math.max(maxSum, sum);
+    		return;
+    	}
+    	
+    	if (level == 1) {
+    		
+        	for (int i = -1; i <= 1; i++) {
+        		for (int j = -1; j <= 2; j++) {
+        			
+        			if (i == -1 && j == -1) continue;		// 각 모서리
+        			if (i == -1 && j == 2) continue;
+        			if (i == 1 && j == -1) continue;
+        			if (i == 1 && j == 2) continue;
+        			
+        			nNext = nNow + i;
+        			mNext = mNow + j;
+        			
+        			if (nNext < 0 || N <= nNext) continue;
+        			if (mNext < 0 || M <= mNext) continue;
+        			if (visited[nNext][mNext]) continue;
+        			
+        			visited[nNext][mNext] = true;
+        			btk2(nNow, mNow, nNext, mNext, cnt + 1, sum + map[nNext][mNext], level + 1);
+        			visited[nNext][mNext] = false;
+        		}//j
+        	}//i
+    		
+    	} else if (level == 2) {
+    		
+    		for (int d = 0; d < 4; d++) {
+    			
+    			int nnNext = nNext + dn[d];
+    			int mmNext = mNext + dm[d];
+    			
+    			if (nnNext < 0 || N <= nnNext) continue;
+    			if (mmNext < 0 || M <= mmNext) continue;
+    			if (visited[nnNext][mmNext]) continue;
+    			
+    			visited[nnNext][mmNext] = true;
+    			btk2(nNow, mNow, nNext, mNext, cnt + 1, sum + map[nnNext][mmNext], level + 1);
+    			visited[nnNext][mmNext] = false;
+    		}//d
+    		
+    	}//if level
+    	
+    }
 }
