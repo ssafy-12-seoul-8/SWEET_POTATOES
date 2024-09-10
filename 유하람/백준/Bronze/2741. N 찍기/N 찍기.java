0280e1b8b4fa65af08f44ce359a96882a735a868
Ryuharam <dlfnadl97@naver.com>
2024-09-11 08:18:10 +0900

    [Bronze V] Title: N 찍기, Time: 844 ms, Memory: 33644 KB -SsafyHub

---
+++ 유하람/백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i=1 ; i<N+1 ; i++) {
+			System.out.println(i);
+		}
+		
+		
+	}
+
+}
