fdcb5b4e8b5b033dcc4eb373733027bcc215728b
Ryuharam <dlfnadl97@naver.com>
2024-09-11 08:53:54 +0900

    [Bronze V] Title: A+B - 4, Time: 184 ms, Memory: 17908 KB -SsafyHub

---
+++ 유하람/백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(A+B);
+			
+		}
+		
+	}
+
+}
