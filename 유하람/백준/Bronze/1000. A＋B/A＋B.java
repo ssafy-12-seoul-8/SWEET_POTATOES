353491e1b4bd27db53e2cb1e96e8976374de1615
Ryuharam <dlfnadl97@naver.com>
2024-09-11 07:59:47 +0900

    [Bronze V] Title: A+B, Time: 168 ms, Memory: 17692 KB -SsafyHub

---
+++ 유하람/백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println(num1+num2);
+		
+	}
+
+}
