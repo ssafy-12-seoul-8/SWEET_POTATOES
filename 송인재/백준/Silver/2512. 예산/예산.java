3af7cd7329e366441170b46dc2ff852e4f29fbb4
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-09-11 15:10:04 +0900

    [Silver II] Title: 예산, Time: 132 ms, Memory: 15108 KB -SsafyHub

--- 송인재/백준/Silver/2512. 예산/예산.java
+++ 송인재/백준/Silver/2512. 예산/예산.java
@@ -1,62 +1,43 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	
-	static int n, min, total, totalBudget, maxBudget;
-	static int[] requests;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		requests = new int[n];
-		min = Integer.MAX_VALUE;
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		for (int i = 0; i < n; i++) {
-			requests[i] = Integer.parseInt(st.nextToken());
-			maxBudget += requests[i];
-		}
-		
-		total = Integer.parseInt(br.readLine());
-		maxBudget = Math.min(total, maxBudget);
-		
-		binarySearch(0, total);
-		System.out.println(min);
-	}
-	
-	static void binarySearch(int left, int right) {
-		if (left > right) {
-			return;
-		}
-		
-		int mid = (left + right) / 2;
-		int budget = allocateBudget(mid);
-		
-		if (budget <= total) {
-			if (budget > totalBudget) {
-				min = mid;
-				totalBudget = budget;
-			} else if (budget == totalBudget) {
-				min = Math.min(mid, min);
-			}
-		}
-		
-		if (budget < maxBudget) {
-			binarySearch(mid + 1, right);
-		} else {
-			binarySearch(left, mid - 1);
-		}
-	}
-	
-	static int allocateBudget(int limit) {
-		int sum = 0;
-		
-		for (int i = 0; i < n; i++) {
-			sum += requests[i] > limit ? limit : requests[i];
-		}
-		
-		return sum;
-	}
-	
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] requests = new int[n];
+        int left = 0;
+        int right = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            requests[i] = Integer.parseInt(st.nextToken());
+            right = Math.max(right, requests[i]);
+        }
+        
+        int m = Integer.parseInt(br.readLine());
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int totalBudget = allocateBudget(requests, mid);
+            
+            if (totalBudget <= m) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        System.out.println(left - 1);
+    }
+    
+    static int allocateBudget(int[] requests, int limit) {
+        int sum = 0;
+        
+        for (int i = 0; i < requests.length; i++) {
+            sum += requests[i] > limit ? limit : requests[i];
+        }
+        
+        return sum;
+    }
 }
