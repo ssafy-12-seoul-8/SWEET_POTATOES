4a2bfec424ab9eaa47d3b18db96bbca1c1174261
Ryuharam <dlfnadl97@naver.com>
2024-09-12 13:49:58 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 164 ms, Memory: 17580 KB -SsafyHub

---
+++ 유하람/백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		System.out.println(str.length());
+		
+		
+		
+		
+	}	// main
+
+}
