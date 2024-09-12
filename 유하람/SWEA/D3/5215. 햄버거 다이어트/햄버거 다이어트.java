900c451c4ec6d77da5c5617511c0771656d1b45e
Ryuharam <dlfnadl97@naver.com>
2024-09-12 13:47:31 +0900

    [D3] Title: 햄버거 다이어트, Time: 210 ms, Memory: 23,700 KB -SsafyHub

--- 유하람/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ 유하람/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,56 +1,57 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-    static int[][] element;
-	static int answer;
-    
-	public static void main(String args[]) throws Exception
-	{
-        Scanner sc = new Scanner(System.in);
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		int T = sc.nextInt();
 		
 		for(int tc=1 ; tc<=T ; tc++) {
 			
-			// 재료의 수 N
-			int N = sc.nextInt();
+			int N = sc.nextInt();	// 재료의 개수
+			int L = sc.nextInt();	// 칼로리 제한
 			
-			// 제한 칼로리 L
-			int L = sc.nextInt();
+			//재료 입력
+			int[][] food = new int[N+1][2];	// 1부터 시작
 			
-			// 재료의 점수와 칼로리를 저장하는 int[][] 배열
-			element = new int[N][2];
-			for(int i=0 ; i<N ; i++) {
-				element[i][0] = sc.nextInt();	// 점수
-				element[i][1] = sc.nextInt();	// 칼로리
+			int min = 100000;
+			int max = 0;
+			
+			for(int i=1 ; i<=N ; i++) {
+				food[i][0] = sc.nextInt();	// 점수
+				food[i][1] = sc.nextInt();	// 칼로리
+				if(food[i][1]>max) max = food[i][1];
+				if(food[i][1]<min) min = food[i][1];
+				
 			}
 			
-			answer = Integer.MIN_VALUE;
+			int[][] dp = new int[N+1][L+1];
 			
-			cal(N, L, 0, 0, 0);
-			
-			System.out.println("#"+tc+" "+answer);
-		}
-	}
-    static void cal(int N, int L, int idx, int score, int cal) {
-		
-		if(idx==N) {
-			if(cal<L) {
-				answer = Math.max(answer, score);
+			// i번 재료를 j 칼로리 제한일 때 사용할 지
+			for(int i=1 ; i<=N ; i++) {
+				for(int j=1 ; j<=L ; j++) {
+
+					if(food[i][1]<=j) {			// 해당 재료 쓸 수 있음
+						// 해당 재료 쓰는 경우
+						int choice1 = dp[i-1][j-food[i][1]] + food[i][0];
+						// 쓰지 않는 경우
+						int choice2 = dp[i-1][j];
+						dp[i][j] = Math.max(choice1, choice2);
+					}else {						// 쓸 수 없음
+						dp[i][j] = dp[i-1][j];
+					}
+				}
 			}
-			return;
+			
+			
+			System.out.println("#"+tc+" "+dp[N][L]);
+			
 		}
 		
-		// 안뽑는 경우
-		cal(N,L,idx+1, score, cal);
-		
-		// 뽑는 경우
-		score += element[idx][0];
-		cal += element[idx][1];
-		cal(N, L, idx+1, score, cal);
-		
 	}
-    
+
 }
