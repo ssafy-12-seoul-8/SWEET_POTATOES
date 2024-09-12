aaab99b375cf79b07d43f775cf32297a5f2a1d44
ciocio <tmddusleo@icloud.com>
2024-09-12 15:05:44 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 18640 KB -SsafyHub

---
+++ 이승연/백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int M;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		dfs(0, 0, "");
+		
+		System.out.println(sb);
+	}
+	
+	static void dfs(int depth, int idx, String result) {
+		if (depth == M) {
+			sb.append(result + "\n");
+			return;
+		}
+		
+		for (int i = idx; i < N; i++) {
+			dfs(depth + 1, i, result + (i + 1) + " ");
+		}
+	}
+}
