425ad685ba0e8bb4c0bac7fde66677c3bb6eefc3
skybory <154962211+skybory@users.noreply.github.com>
2024-09-12 11:50:05 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 100 ms, Memory: 14120 KB -SsafyHub

---
+++ 김태연/백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		String result;
+		if (A > B) {
+			result = ">";
+		} else if (A<B) {
+			result = "<";
+		} else {
+			result = "==";
+		}
+		
+		System.out.println(result);
+	}
+	
+}
