33fb487029242d70918f75c3dbc9878a2e20e8d8
skybory <154962211+skybory@users.noreply.github.com>
2024-09-12 11:26:58 +0900

    [Bronze V] Title: A/B, Time: 168 ms, Memory: 17792 KB -SsafyHub

---
+++ 김태연/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		double result = A/B;
+		System.out.println(result);
+	}
+}
