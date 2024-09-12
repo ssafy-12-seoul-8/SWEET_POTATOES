087f8a2b972882661aee8656c10a1dfdd9efe79a
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-09-12 11:27:44 +0900

    [Silver I] Title: 기타 레슨, Time: 316 ms, Memory: 23948 KB -SsafyHub

---
+++ 송인재/백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int n, m;
+	static int[] courses;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		courses = new int[n];
+		int left = 0;
+		int right = 10_000 * n;
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			courses[i] = Integer.parseInt(st.nextToken());
+			left = Math.max(left, courses[i]);
+		}
+		
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			
+			if (isPossible(mid)) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		
+		System.out.println(left);
+	}
+	
+	static boolean isPossible(int limit) {
+		int count = 0;
+		int sum = 0;
+		
+		for (int i = 0; i < n; i++) {
+			if (sum + courses[i] > limit) {
+				count++;
+				sum = 0;
+			}
+			
+			if (count > m) {
+				return false;
+			}
+			
+			sum += courses[i];
+		}
+		
+		if (sum <= limit && count + 1 > m) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+}
