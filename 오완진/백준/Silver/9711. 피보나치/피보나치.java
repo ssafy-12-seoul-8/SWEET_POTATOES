c5492780afc199e237fed9b0daa25e0bd10ab895
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-22 18:37:53 +0900

    [Silver III] Title: 피보나치, Time: 680 ms, Memory: 50936 KB -SsafyHub

---
+++ 오완진/백준/Silver/9711. 피보나치/피보나치.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static long N, M;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int TC = sc.nextInt();
+		for (int tc = 1; tc <= TC; tc++) {
+
+			N = sc.nextLong();
+			M = sc.nextLong();
+			
+			long[] result = fibo(N);
+			System.out.println("Case #" + tc + ": " + result[0]);
+			
+		}
+	}
+	
+	static long[] fibo(long n) {
+		if (n == 0)
+			return new long[] {0, 1};
+
+		/*
+		 * fb(2k) = fb(k)*(2fb(k+1) - fb(k))
+		 * fb(2k+1) = fb(k+1)^2 + fb(k))^2
+		 */
+		
+		long[] newFibb = fibo(n / 2);
+		
+		long fbK = newFibb[0];
+		long fbKplus1 = newFibb[1];
+		
+        long fb2K = (fbK * ((2 * fbKplus1 % M - fbK + M) % M)) % M;
+        long fb2Kplus1 = (fbKplus1 * fbKplus1 % M + fbK * fbK % M) % M;
+        
+        if (n % 2 == 0)
+        	return new long[] {fb2K, fb2Kplus1};
+        else
+        	return new long[] {fb2Kplus1, (fb2K + fb2Kplus1) % M};
+    }
+	
+}
