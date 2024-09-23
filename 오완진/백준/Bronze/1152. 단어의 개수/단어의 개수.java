641124b5ba3559d8a373bca421cd66a6543ed214
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-23 22:47:00 +0900

    [Bronze II] Title: 단어의 개수, Time: 428 ms, Memory: 30744 KB -SsafyHub

---
+++ 오완진/백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	String str = sc.nextLine();
+    	
+    	StringTokenizer st = new StringTokenizer(str," ");
+    	
+    	System.out.println(st.countTokens());	
+    	
+    }
+}
