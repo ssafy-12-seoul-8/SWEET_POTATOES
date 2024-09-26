9d1f2452e8d71861050aa58f0ed8894d32c3dbd3
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-26 21:57:10 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 13076 KB -SsafyHub

---
+++ 오완진/백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		int[] coins = new int[N+1];
+		for (int i = 1; i <= N; i++)
+			coins[i] = sc.nextInt();
+		
+		int[] dp = new int[K+1];
+		dp[0] = 1;
+		
+        for (int i = 1; i <= N; i++) {
+        	int coin = coins[i];
+        	for (int j = coin; j <= K; j++)
+        		dp[j] += dp[j - coin];
+        }
+		
+		System.out.println(dp[K]);
+	}
+}
