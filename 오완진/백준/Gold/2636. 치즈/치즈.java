6fc86d23422a0dfd2c658e56a82cb0d6834582f0
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-26 23:17:20 +0900

    [Gold IV] Title: 치즈, Time: 188 ms, Memory: 21756 KB -SsafyHub

---
+++ 오완진/백준/Gold/2636. 치즈/치즈.java
@@ -0,0 +1,90 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int[] dn = {-1, 0, 1, 0};
+	static int[] dm = {0, 1, 0, -1};
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[][] map = new int[N][M];
+		for (int n = 0; n < N; n++)
+			for (int m = 0; m < M; m++)
+				map[n][m] = sc.nextInt();
+		
+		int time = 0;
+		int cheeseCntMemo = 0;
+		while (true) {
+			
+			Queue<int[]> bfs = new LinkedList<>();
+			Queue<int[]> melting = new LinkedList<>();
+			boolean[][] visited = new boolean[N][M];
+			
+			bfs.add(new int[] {0, 0});
+			visited[0][0] = true;
+			
+			while (!bfs.isEmpty()) {
+				
+				int[] now = bfs.poll();
+				int nNow = now[0];
+				int mNow = now[1];
+				
+				int cnt = 0;
+				for (int d = 0; d < 4; d++) {
+					int nNext = nNow + dn[d];
+					int mNext = mNow + dm[d];
+					
+					if (nNext < 0 || N <= nNext || mNext < 0 || M <= mNext || visited[nNext][mNext])
+						continue;
+					
+					if (map[nNext][mNext] == 0) {
+						bfs.add(new int[] {nNext, mNext});
+						visited[nNext][mNext] = true;
+					}
+				}
+				
+			}//bfs
+			
+			int cheeseCnt = 0;
+			for (int n = 0; n < N; n++) {
+				for (int m = 0; m < M; m++) {
+					
+					if (map[n][m] == 1) {
+						cheeseCnt++;
+						
+						boolean contact = false;
+						for (int d = 0; d < 4; d++) {
+							int nNext = n + dn[d];
+							int mNext = m + dm[d];
+							
+							if (visited[nNext][mNext])
+								contact = true;
+						}
+						
+						if (contact)
+							melting.add(new int[] {n, m});
+					}
+				}
+			}
+			
+			if (!melting.isEmpty())
+				while (!melting.isEmpty()) {
+					int[] cheese = melting.poll();
+					map[cheese[0]][cheese[1]] = 0;
+					cheeseCntMemo = cheeseCnt;
+				}
+			else break;
+			time++;
+		}
+		
+		System.out.println(time);
+		System.out.println(cheeseCntMemo);
+	}
+}
