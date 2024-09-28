f8b14128bde4683d23d279bc47e8963271d84357
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-28 16:33:58 +0900

    [D4] Title: 치즈 도둑, Time: 561 ms, Memory: 108,108 KB -SsafyHub

--- 오완진/SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ 오완진/SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class Solution {
 	
-	static int N;
-	static int[][] cheese;
-	static boolean[][] visited;
-	static int maxCnt;
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
+	
+	static int N, maxCnt;
+	static int[][] map;
+	
+	static boolean noMoreCheese;
 	
 	public static void main(String[] args) {
 		
@@ -19,50 +20,77 @@ public class Solution {
 		for (int tc = 1; tc <= TC; tc++) {
 			
 			N = sc.nextInt();
-			cheese = new int[N][N];
+			map = new int[N][N];
 			for (int r = 0; r < N; r++)
 				for (int c = 0; c < N; c++)
-					cheese[r][c] = sc.nextInt();
+					map[r][c] = sc.nextInt();
+			
 			maxCnt = 0;
-
-			for (int day = 0; day <= 100; day++) {	// = cheese[r][c]
-				int cnt = 0;
-				visited = new boolean[N][N];
-
-				for (int r = 0; r < N; r++) {
-					for (int c = 0; c < N; c++) {
-
-						Queue<int[]> bfs = new LinkedList<>();
+			noMoreCheese = false;
+			
+			for (int t = 0; t <= 100; t++) {
+				
+				if (t == 0) {
+					cntCheese();
+					continue;
+				}
+				
+				callAngel(t);
+				cntCheese();
+				
+				if (noMoreCheese) break;
+			}
+			
+			System.out.println("#" + tc + " " + maxCnt);
+		}//tc
+		
+	}
+	
+	static void callAngel(int t) {
+		for (int r = 0; r < N; r++)
+			for (int c = 0; c < N; c++)
+				if (map[r][c] <= t) map[r][c] = 0;
+	}
+	
+	static void cntCheese() {
+		
+		Queue<int[]> bfs = new LinkedList<>();
+		boolean[][] visited = new boolean[N][N];
+		
+		int cnt = 0;
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				
+				if (map[r][c] > 0 && !visited[r][c]) {
+					
+					bfs.add(new int[] {r, c});
+					visited[r][c] = true;
+					cnt++;
+					
+					while (!bfs.isEmpty()) {
 						
-						if (cheese[r][c] > day && !visited[r][c]) {
-							bfs.add(new int[] {r, c});
-							visited[r][c] = true;
+						int[] now = bfs.poll();
+						int rNow = now[0];
+						int cNow = now[1];
+						
+						for (int d = 0; d < 4; d++) {
+							int rNext = rNow + dr[d];
+							int cNext = cNow + dc[d];
 							
-							while (!bfs.isEmpty()) {
-								int[] now = bfs.poll();
-								int rNow = now[0];
-								int cNow = now[1];
-								
-								for (int d = 0; d < 4; d++) {
-									int rNext = rNow + dr[d];
-									int cNext = cNow + dc[d];
-									
-									if (rNext < 0 || N <= rNext) continue;
-									if (cNext < 0 || N <= cNext) continue;
-									if (cheese[rNext][cNext] <= day) continue;
-									if (visited[rNext][cNext]) continue;
-									
-									bfs.add(new int[] {rNext, cNext});
-									visited[rNext][cNext] = true;
-								}
-							}
-							cnt++;
+							if (rNext < 0 || N <= rNext || cNext < 0 || N <= cNext ||
+									map[rNext][cNext] == 0 || visited[rNext][cNext]) continue;
+							
+							bfs.add(new int[] {rNext, cNext});
+							visited[rNext][cNext] = true;
 						}
 					}
+					
 				}
-				maxCnt = Math.max(maxCnt, cnt);
+				
 			}
-			System.out.println("#" + tc + " " + maxCnt);
 		}
+
+		if (cnt == 0) noMoreCheese = true;
+		maxCnt = Math.max(maxCnt, cnt);
 	}
 }
