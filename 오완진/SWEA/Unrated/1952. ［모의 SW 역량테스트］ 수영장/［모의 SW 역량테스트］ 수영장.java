be3e7760c887dcdcfabc10f2f4050c239e54fddc
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-28 15:27:01 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 168 ms, Memory: 24,192 KB -SsafyHub

--- 오완진/SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ 오완진/SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,11 +1,6 @@
 import java.util.Scanner;
 
 public class Solution {
-
-	static int[] price;
-	static int[] month;
-	static int minCost;
-	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -13,40 +8,24 @@ public class Solution {
 		int TC = sc.nextInt();
 		for (int tc = 1; tc <= TC; tc++) {
 			
-			price = new int[4];	// 1일 1달 3달 1년
-			month = new int[12];
+			int[] price = new int[4];
+			for (int i = 0; i < 4; i++)
+				price[i] = sc.nextInt();
 			
-			for (int p = 0; p < 4; p++)
-				price[p] = sc.nextInt();
-			for (int m = 0; m < 12; m++)
-				month[m] = sc.nextInt();
-
-			minCost = 1 * price[3];
-
-			btk(0, 0);
+			int[] month = new int[15];
+			for (int i = 3; i <= 14; i++) {
+				int count = sc.nextInt();
+				month[i] = Math.min(count * price[0], price[1]);
+			}
 			
-			System.out.println("#" + tc + " " + minCost);
-		}
+			int[] dp = new int[15];
+			for (int i = 3; i <= 14; i++) {
+				dp[i] = Math.min(dp[i-3] + price[2], dp[i-1] + month[i]);
+			}
+			
+			int bestPrice = Math.min(price[3], dp[14]);
+			System.out.println("#" + tc + " " + bestPrice);
+		}//tc
 		
 	}
-	
-	static void btk(int monthIdx, int cost) {
-		
-		if (monthIdx >= 12) {
-			minCost = Math.min(minCost, cost);
-			return;
-		}
-		
-		if (month[monthIdx] == 0) {
-			btk(monthIdx + 1, cost);
-			return;
-		}
-		// 1일권
-		btk(monthIdx + 1, cost + month[monthIdx] * price[0]);
-		// 1달권
-		btk(monthIdx + 1, cost + price[1]);
-		// 3달권
-		btk(monthIdx + 3, cost + price[2]);
-	}
-	
 }
