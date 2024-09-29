3cccef0dd895cdf5f81246ed9941bb760b4bca3b
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-09-30 08:43:59 +0900

    [D3] Title: 햄버거 다이어트, Time: 205 ms, Memory: 31,716 KB -SsafyHub

--- 송인재/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ 송인재/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,12 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	
-	static int n, l, max;
-	static int[] scores, calories;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,34 +9,30 @@ public class Solution {
 		
 		for (int t = 1; t <= cases; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			l = Integer.parseInt(st.nextToken());
-			scores = new int[n];
-			calories = new int[n];
-			max = 0;
+			int n = Integer.parseInt(st.nextToken());
+			int l = Integer.parseInt(st.nextToken());
+			int[][] hamburgers = new int[n + 1][2];
+			int[][] dp = new int[n + 1][l + 1];
 			
-			for (int i = 0; i < n; i++) {
+			for (int i = 1; i <= n; i++) {
 				st = new StringTokenizer(br.readLine());
-				scores[i] = Integer.parseInt(st.nextToken());
-				calories[i] = Integer.parseInt(st.nextToken());
+				hamburgers[i][0] = Integer.parseInt(st.nextToken());
+				hamburgers[i][1] = Integer.parseInt(st.nextToken());
 			}
 			
-			combination(0, 0, 0);
-			System.out.println("#" + t + " " + max);
-		}
-		
-	}
-	
-	static void combination(int index, int totalScore, int totalCalories) {
-		if (totalCalories > l) {
-			return;
-		}
-		
-		max = Math.max(max, totalScore);
-		
-		for (int i = index; i < n; i++) {
-			combination(i + 1, totalScore + scores[i], totalCalories + calories[i]);
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= l; j++) {
+					if (hamburgers[i][1] > j) {
+						dp[i][j] = dp[i - 1][j];
+					} else {
+						int contain = dp[i - 1][j - hamburgers[i][1]] + hamburgers[i][0];
+						dp[i][j] = contain > dp[i - 1][j] ? contain : dp[i - 1][j];
+					}
+				}
+			}
+			
+			System.out.println("#" + t + " " + dp[n][l]);
 		}
 	}
-	
+
 }
