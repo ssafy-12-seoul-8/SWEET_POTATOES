2fd69dcaabe685343bc29b5f46f724565f84784e
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-09-30 08:01:18 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 166 ms, Memory: 19,272 KB -SsafyHub

--- 송인재/SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ 송인재/SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,63 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-
-	static final int[] prices = new int[4];
-	static final int[] plan = new int[12];
-	static final int[][] dp = new int[4][12];
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int cases = Integer.parseInt(br.readLine());
-
+		
 		for (int t = 1; t <= cases; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-
+			int[] price = new int[4];
+			
 			for (int i = 0; i < 4; i++) {
-				prices[i] = Integer.parseInt(st.nextToken());
+				price[i] = Integer.parseInt(st.nextToken());
 			}
-
+			
+			int[] monthlyPrice = new int[13];
 			st = new StringTokenizer(br.readLine());
-
-			for (int i = 0; i < 12; i++) {
-				plan[i] = Integer.parseInt(st.nextToken());
-			}
-
-			for (int i = 0; i < 4; i++) {
-				for (int j = 0; j < 12; j++) {
-					int daily = prices[0] * plan[j];
-
-					switch (i) {
-					case 0:
-						dp[i][j] = j != 0 ? daily + dp[i][j - 1] : daily;
-						break;
-					case 1:
-						dp[i][j] = j != 0 ? Math.min(prices[i] + dp[i][j - 1], dp[i][j - 1] + daily)
-								: Math.min(dp[i - 1][j], prices[i]);
-						dp[i][j] = Math.min(dp[i][j], dp[i - 1][j]);
-						break;
-					case 2:
-						dp[i][j] = j >= 3 ? Math.min(dp[i][j - 3] + prices[i], dp[i][j - 1] + daily)
-								: Math.min(dp[i - 1][j], prices[i]);
-
-						if (j != 0) {
-							dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + prices[1]);
-						}
-
-						dp[i][j] = Math.min(dp[i][j], dp[i - 1][j]);
-						break;
-					case 3:
-						dp[i][j] = Math.min(prices[i], dp[i - 1][j]);
-					default:
-						break;
-					}
+			
+			for (int i = 1; i < 13; i++) {
+				int monthUse = Integer.parseInt(st.nextToken());
+				monthlyPrice[i] = Math.min(monthlyPrice[i - 1] + monthUse * price[0], monthlyPrice[i - 1] + price[1]);
+				
+				if (i >= 3) {
+					monthlyPrice[i] = Math.min(monthlyPrice[i], monthlyPrice[i - 3] + price[2]);
 				}
 			}
+			
+			int finalPrice = Math.min(monthlyPrice[12], price[3]);
+			
+			System.out.println("#" + t + " " + finalPrice);
 
-			System.out.println("#" + t + " " + dp[3][11]);
 		}
 	}
 
