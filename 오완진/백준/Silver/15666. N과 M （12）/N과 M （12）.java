2a0c956f5a2e12652acebaf4f98f543a0810f020
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-30 03:27:08 +0900

    [Silver II] Title: N과 M (12), Time: 104 ms, Memory: 13768 KB -SsafyHub

--- 오완진/백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 오완진/백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -1,51 +1,57 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static int N;
-	static int M;
-	static Queue<Integer>[] NN;				// 기존 visited 상위호환
+	
+	static int N, M;
+//	static int[] NN;
+	static HashSet<Integer> keySet;
+	static List<Integer> arr, keyArr;
+	static Queue<Integer>[] NN;
+	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
+	
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		NN = new Queue[10000];
-		for (int i = 0; i < 10000; i++)
-			NN[i] = new LinkedList<>();
-		
-		for (int n = 0; n < N; n++) {
-			int num = sc.nextInt();
-			NN[num].add(0);					// 의미없는 0임
-		}
-		
-		btk(0, new ArrayList<>());
-		
-	}
-	
-	static void btk(int start, List<Integer> pickN) {
-		if (pickN.size() == M) {
-			for (int num : pickN)
-				System.out.print(num + " ");
-			System.out.println();
-			
-			return;
-		}
-		
-		for (int n = start; n < 10000; n++) {
-			if (!NN[n].isEmpty()) {
-//				NN[n].poll();
-				pickN.add(n);
-				btk(n, pickN);
-				pickN.remove(pickN.size() - 1);
-//				NN[n].add(0);
-			}
-		}
-	}
-	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	N = sc.nextInt();
+    	M = sc.nextInt();
+    	
+    	keySet = new HashSet<>();
+    	for (int i = 0; i < N; i++)
+    		keySet.add(sc.nextInt());
+    		
+    	keyArr = new ArrayList<>(keySet);
+    	Collections.sort(keyArr);
+    	
+    	arr = new ArrayList<>();
+    	visited = new boolean[N+1];
+    	    	
+    	btk(0);
+    	System.out.println(sb);
+    }
+    
+    static void btk(int idx) {
+    	
+    	if (arr.size() == M) {
+    		for (int num : arr)
+    			sb.append(num).append(" ");
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	for (int i = idx; i < keyArr.size(); i++) {
+    		
+    		int key = keyArr.get(i);
+    		arr.add(key);
+    		btk(i);
+    		arr.remove(arr.size() - 1);
+    	}
+    	
+    }
 }
