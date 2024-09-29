569cb50e8a7e6d58bc8ef1db8883008d2f03bc06
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-30 02:35:35 +0900

    [Silver III] Title: N과 M (1), Time: 1540 ms, Memory: 56284 KB -SsafyHub

--- 오완진/백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 오완진/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	
-	static int N;
-	static int M;
+	static int N, M;
+	static int[] arr;
 	static boolean[] visited;
 	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		visited = new boolean[N];
-		
-		btk(new ArrayList<>());
-	}
-	
-	static void btk(List<Integer> pickN) {
-		if(pickN.size() == M) {
-			for (int num : pickN)
-				System.out.print(num + " ");
-			System.out.println();
-			
-			return;
-		}
-		
-		for (int n = 0; n < N; n++) {
-			if (!visited[n]) {
-				pickN.add(n+1);
-				visited[n] = true;
-				btk(pickN);
-				visited[n] = false;
-				pickN.remove(pickN.size() - 1);
-			}
-		}
-		
-	}
+    public static void main(String[] args) {
+
+    	Scanner sc = new Scanner(System.in);
+    	
+    	N = sc.nextInt();
+    	M = sc.nextInt();
+    	
+    	arr = new int[M];
+    	visited = new boolean[N+1];
+    	    	
+    	btk(0);
+    }
+    
+    static void btk(int idx) {
+    	
+    	if (idx == M) {
+    		for (int num : arr)
+    			System.out.print(num + " ");
+    		System.out.println();
+    		return;
+    	}
+    	
+    	for (int i = 1; i <= N; i++) {
+    		
+    		if (!visited[i]) {
+    			visited[i] = true;
+    			arr[idx] = i;
+    			btk(idx + 1);
+    			visited[i] = false;
+    		}
+    		
+    	}
+    	
+    }
 }
