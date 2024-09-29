c9704a026da788f8dfd044deb5f601de899f4bea
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-30 03:24:28 +0900

    [Silver II] Title: N과 M (11), Time: 436 ms, Memory: 56016 KB -SsafyHub

--- 오완진/백준/Silver/15665. N과 M （11）/N과 M （11）.java
+++ 오완진/백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
@@ -7,43 +8,50 @@ import java.util.Scanner;
 public class Main {
 	
 	static int N, M;
-	static Queue<Integer>[] NN = new Queue[10000];
+//	static int[] NN;
+	static HashSet<Integer> keySet;
+	static List<Integer> arr, keyArr;
+	static Queue<Integer>[] NN;
+	static boolean[] visited;
 	static StringBuilder sb = new StringBuilder();
-
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		for (int n = 0; n < 10000; n++)
-			NN[n] = new LinkedList<>();
-		
-		for (int n = 0; n < N; n++)
-			NN[sc.nextInt()].add(0);
-		
-		btk(new ArrayList<>());
-		
-		System.out.println(sb);
-	}
 	
-	static void btk(List<Integer> pickN) {
-		if(pickN.size() == M) {
-			for (int num : pickN)
-                sb.append(num).append(" ");
-            sb.append("\n");
-			
-			return;
-		}
-		
-		for (int n = 0; n < 10000; n++) {
-			if (!NN[n].isEmpty()) {
-				pickN.add(n);
-				btk(pickN);
-				pickN.remove(pickN.size() - 1);
-			}
-		}
-		
-	}
+    public static void main(String[] args) {
+
+    	Scanner sc = new Scanner(System.in);
+    	
+    	N = sc.nextInt();
+    	M = sc.nextInt();
+    	
+    	keySet = new HashSet<>();
+    	for (int i = 0; i < N; i++)
+    		keySet.add(sc.nextInt());
+    		
+    	keyArr = new ArrayList<>(keySet);
+    	Collections.sort(keyArr);
+    	
+    	arr = new ArrayList<>();
+    	visited = new boolean[N+1];
+    	    	
+    	btk();
+    	System.out.println(sb);
+    }
+    
+    static void btk() {
+    	
+    	if (arr.size() == M) {
+    		for (int num : arr)
+    			sb.append(num).append(" ");
+    		sb.append("\n");
+    		return;
+    	}
+    	
+    	for (int i = 0; i < keyArr.size(); i++) {
+    		
+    		int key = keyArr.get(i);
+    		arr.add(key);
+    		btk();
+    		arr.remove(arr.size() - 1);
+    	}
+    	
+    }
 }
