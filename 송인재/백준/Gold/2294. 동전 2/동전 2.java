321d63d38fdad1770c49d8b4ee6b1dc89bb0c74d
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-10-06 01:11:29 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 14140 KB -SsafyHub

--- 송인재/백준/Gold/2294. 동전 2/동전 2.java
+++ 송인재/백준/Gold/2294. 동전 2/동전 2.java
@@ -20,11 +20,9 @@ public class Main {
 
     dp[0] = 0;
 
-    for (int i = 1; i <= k; i++) {
-      for (int j = 0; j < n; j++) {
-        if (coins[j] <= i) {
-          dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-        }
+    for (int i = 0; i < n; i++) {
+      for (int j = coins[i]; j <= k; j++) {
+        dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
       }
     }
 
