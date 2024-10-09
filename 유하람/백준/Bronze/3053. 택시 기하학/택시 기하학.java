b0bc1e9801304aab92807540b976d6da44299425
Ryuharam <dlfnadl97@naver.com>
2024-10-10 08:16:25 +0900

    [Bronze III] Title: 택시 기하학, Time: 68 ms, Memory: 11604 KB -SsafyHub

---
+++ 유하람/백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static List<Integer> restPlace;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int R = Integer.parseInt(br.readLine());
+		
+		System.out.println(Math.PI*Math.pow(R, 2));
+		System.out.println(2*Math.pow(R, 2));
+		
+		
+		
+	} // main
+
+} // Main class
