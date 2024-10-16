7f297ca364d2e78b4f97f7cb68926c27b7b8d0be
ciocio <tmddusleo@icloud.com>
2024-10-16 12:03:12 +0900

    [Gold II] Title: Convention II, Time: 476 ms, Memory: 42956 KB -SsafyHub

--- 이승연/백준/Gold/16767. Convention II/Convention II.java
+++ 이승연/백준/Gold/16767. Convention II/Convention II.java
@@ -49,26 +49,22 @@ public class Main {
 		
 		T = min;
 		
-		while (!pqA.isEmpty() || !pqO.isEmpty()) {
-
-            // 도착 시간이 현재 시간 이전이거나 같은 소들을 대기 큐(pqO)에 추가
-            while (!pqA.isEmpty() && pqA.peek().arrivalTime <= T) {
-                pqO.add(pqA.poll());
-            }
-
-            // 대기 중인 소가 없다면, 다음 소의 도착 시간으로 T를 갱신
-            if (pqO.isEmpty()) {
-                if (!pqA.isEmpty()) {
-                    T = pqA.peek().arrivalTime; // 다음 소가 도착할 시간으로 설정
-                    continue;
-                }
-            }
-
-            // 대기 중인 소가 있다면, 순서대로 소를 꺼내어 처리
-            cow currCow = pqO.poll();
-            maxWaitingTime = Math.max(maxWaitingTime, T - currCow.arrivalTime); // 대기 시간 계산
-            T += currCow.eatingTime; // 소가 먹고 있는 시간을 더함
-        }
+		while(!pqA.isEmpty() || !pqO.isEmpty()) {
+			
+			while(!pqA.isEmpty() && pqA.peek().arrivalTime <= T) {
+				pqO.add(pqA.poll());
+			}
+			
+			if (pqO.isEmpty() && !pqA.isEmpty()) {
+				T = pqA.peek().arrivalTime;
+				continue;
+			}
+			
+			cow currCow = pqO.poll();
+			
+			maxWaitingTime = Math.max(maxWaitingTime, T - currCow.arrivalTime);
+			T += currCow.eatingTime;
+		}
 		
 		System.out.println(maxWaitingTime);
 	}
