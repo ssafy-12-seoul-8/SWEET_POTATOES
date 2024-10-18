8ab666336be88907037d2c2e05c80f5cbab0dc2b
Ryuharam <dlfnadl97@naver.com>
2024-10-18 23:32:37 +0900

    [Bronze V] Title: 웰컴, Time: 64 ms, Memory: 11260 KB -SsafyHub

---
+++ 유하람/백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println(".  .   .\n"
+				+ "|  | _ | _. _ ._ _  _\n"
+				+ "|/\\|(/.|(_.(_)[ | )(/.");
+
+	} // main
+
+} // Main class
