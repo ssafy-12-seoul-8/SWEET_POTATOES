6b9a5ff007b676f308e281dde7d88fca6df99153
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-10-24 09:34:10 +0900

    [Gold II] Title: ㅋㅋ루ㅋㅋ, Time: 300 ms, Memory: 34188 KB -SsafyHub

--- 송인재/백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.java
+++ 송인재/백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.java
@@ -69,33 +69,3 @@ public class Main {
 	}
 	
 }
-
-/*
- * RRKKRKRKRKRKKRR => K찾기
- * KKRKRKRKRKK => left = k, right = k -> 다음
- * (K)KRKRKRKRK(K) => left = k, right = k -> 다음
- * (KK)RKRKRKR(KK) => left = r, right = r -> r 줄이기
- * (KK)KRKRK(KK) => left = k, right = k -> 다음
- * (KKK)RKR(KKK) => left = r, right = r -> r 없음 -> 3개씩 + 2개 -> 8개
- * R 갯수 =8개
- * 
- * KKKRKRRKRKRKRKRKK => left = k, right = k, rCount = 7 -> 0 + 7 = 7
- * (K)KKRKRRKRKRKRKRK(K) => left = k, right = k, rCount = 7 -> 2 + 7 = 9
- * (KK)KRKRRKRKRKRKR(KK) => left = k, right = r, rCount = 7 -> 4 + 7 = 11
- * (KK)KRKRRKRKRKRK(KK) => left = k, right = k, rCount = 6 -> 4 + 6 = 10
- * (KKK)RKRRKRKRKR(KKK) => left = r, right = r, rCount = 6 -> 6 + 6 = 12
- * (KKK)KRRKRKRK(KKK) => left = k, right = k, rCount = 4 -> 6 + 4 = 10
- * (KKKK)RRKRKR(KKKK) => left = r, right = r, rCount = 4 -> 8 + 4 = 12
- * (KKKK)RKRK(KKKK) => left = r, right = k, rCount = 2 -> 8 + 2 = 10
- * (KKKK)KRK(KKKK) => left = k, right = k, rCount = 1 -> 8 + 1 = 9
- * (KKKKK)R(KKKKK) => 11
- * 
- * KRKKRKRRRKKKRKK => left = k, right = k, rCount = 6 -> 0 + 6 = 6
- * (K)RKKRKRRRKKKRK(K) => left = k, right = r, rCount = 6 -> 2 + 6 = 8
- * (K)KKRKRRRKKKRK(K) => left = k, right = k, rCount = 5 -> 2 + 5 = 7
- * (KK)KRKRRRKKKR(KK) => left = k, right = r, rCount = 5 -> 4 + 5 = 9
- * (KK)KRKRRRKKK(KK) => left = k, right = k, rCount = 4 -> 4 + 4 = 8
- * (KKK)RKRRRKK(KKK) => left = r, right = k, rCount = 4 -> 6 + 4 = 10
- * (KKK)KRRRKK(KKK) => left = k, right = k, rCount = 3 -> 6 + 3 = 9
- * (KKKK)RRRK(KKKK) => left = r, right = k, rCount = 3 -> 8 + 3 = 11
- */
