28edbd7a544708dc6945cda962e680ad71dae5fe
Ryuharam <dlfnadl97@naver.com>
2024-10-26 22:51:43 +0900

    [Bronze V] Title: 등록, Time: 64 ms, Memory: 11220 KB -SsafyHub

---
+++ 유하람/백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("170");
+		System.out.println("dlfnadl97");
+		
+		
+	} // main
+
+}
+// Main class
