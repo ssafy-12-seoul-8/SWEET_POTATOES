0194e0195cb397b843545f9db182b8fb9a5321b7
Injae Song <110002292+IjjS@users.noreply.github.com>
2024-11-08 10:42:39 +0900

    [Gold IV] Title: 리그전 오브 레전드, Time: 1648 ms, Memory: 91536 KB -SsafyHub

---
+++ 송인재/백준/Gold/23327. 리그전 오브 레전드/리그전 오브 레전드.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
+		int[] popSum = new int[n];
+		long[] popMul = new long[n];
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			popSum[i] = Integer.parseInt(st.nextToken());
+			popMul[i] = (long) popSum[i] * popSum[i];
+			
+			if (i > 0) {
+				popSum[i] += popSum[i - 1];
+				popMul[i] += popMul[i - 1];
+			}
+		}
+		
+		for (int i = 0; i < q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int l = Integer.parseInt(st.nextToken()) - 1;
+			int r = Integer.parseInt(st.nextToken()) - 1;
+			
+			long sum = popSum[r];
+			long mul = popMul[r];
+			
+			if (l > 0) {
+				sum -= popSum[l - 1];
+				mul -= popMul[l - 1];
+			}
+			
+			long fun = (sum * sum - mul) / 2; 
+			
+			System.out.println(fun);
+		}
+	}
+	
+}
