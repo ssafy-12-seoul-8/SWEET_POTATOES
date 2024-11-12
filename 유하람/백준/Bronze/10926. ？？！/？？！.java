07af1e63d4ba474ee650b3ddaf39aac5e31a1eca
Ryuharam <dlfnadl97@naver.com>
2024-11-13 08:25:05 +0900

    [Bronze V] Title: ??!, Time: 64 ms, Memory: 11532 KB -SsafyHub

---
+++ 유하람/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String id = br.readLine();
+		
+		System.out.println(id+"??!");
+		
+		
+		
+	}	// main
+
+
+}	// Main class
