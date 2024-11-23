5209631d26886aa5066232181a5f4088fcb743a4
Ryuharam <dlfnadl97@naver.com>
2024-11-23 11:43:37 +0900

    [Bronze V] Title: 홍익대학교, Time: 92 ms, Memory: 12884 KB -SsafyHub

---
+++ 유하람/백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        System.out.println(n-1946);
+        
+        
+    }	// main
+
+}	// Main class
