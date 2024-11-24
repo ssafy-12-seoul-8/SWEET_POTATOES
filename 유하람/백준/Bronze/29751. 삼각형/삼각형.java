7b55a3cf9b37da254e75b135266c3b8b0181309a
Ryuharam <dlfnadl97@naver.com>
2024-11-24 09:52:53 +0900

    [Bronze V] Title: 삼각형, Time: 104 ms, Memory: 14624 KB -SsafyHub

---
+++ 유하람/백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main (String[] args) {
+        		Scanner sc = new Scanner(System.in);
+		
+		float a = sc.nextFloat();
+		int b = sc.nextInt();
+		float ans = a*b/2;
+		System.out.println(ans);
+		
+    }
+}
