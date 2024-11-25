7beca36d44302d7500022934eaa49731b297786b
Ryuharam <dlfnadl97@naver.com>
2024-11-26 07:54:10 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 60 ms, Memory: 11244 KB -SsafyHub

---
+++ 유하람/백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("       _.-;;-._\n"
+        		+ "'-..-'|   ||   |\n"
+        		+ "'-..-'|_.-;;-._|\n"
+        		+ "'-..-'|   ||   |\n"
+        		+ "'-..-'|_.-''-._|");
+        
+    }
+}
