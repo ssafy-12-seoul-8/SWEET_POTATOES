3d3967a61018bd19d18a27b42d804e1f5905ec9b
Ryuharam <dlfnadl97@naver.com>
2025-01-14 21:57:38 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 68 ms, Memory: 11508 KB -SsafyHub

---
+++ 유하람/백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(Integer.parseInt(br.readLine())-543);
+
+
+	} // main
+
+} // Main
