70e0a76697ebf12ebefa7d841066cb6f217ce2e3
junyoung <jylee0619@naver.com>
2025-01-26 16:48:28 +0900

    [Silver IV] Title: 지뢰 찾기, Time: 88 ms, Memory: 108384 KB -SsafyHub

--- 이준영/백준/Silver/4396. 지뢰 찾기/지뢰 찾기.py
+++ 이준영/백준/Silver/4396. 지뢰 찾기/지뢰 찾기.py
@@ -1,18 +1,18 @@
-dy = [0, -1, -1, -1, 0, 1, 1, 1]
-dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, -1, -1, -1, 0, 1, 1, 1]        # 지뢰가 몇개 있는지 세기 위한 방향 설정
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]        
 
 n = int(input())
 
 arr = [[0] * n for _ in range(n)]
 click = [[0] * n for _ in range(n)]
-result = [["."] * n for _ in range(n)]
+result = [["."] * n for _ in range(n)]  # 일단 "."으로 초기화하고 바꿀 부분만 바꾸도록 했습니다.
 
 for i in range(n):
     arr[i] = list(input())
 for i in range(n):
     click[i] = list(input())
 
-bomb_check = False
+bomb_check = False  # 지뢰를 밟았는지 체크하는 변수
 
 for i in range(n):
     for j in range(n):
@@ -24,7 +24,7 @@ for i in range(n):
 for i in range(n):
     for j in range(n):
         if click[i][j] == "x":
-            if arr[i][j] == ".":
+            if arr[i][j] == ".":                 # 지뢰 안 밟으면 근처 지뢰 갯수 델타 탐색으로 구해서 출력
                 count = 0
                 for k in range(8):
                     ny = i + dy[k]
@@ -32,10 +32,10 @@ for i in range(n):
                     if 0 <= ny < n and 0 <= nx < n and arr[ny][nx] == "*":
                         count += 1
                 result[i][j] = count
-            if arr[i][j] == "*":
+            if arr[i][j] == "*":                 # 지뢰 밟으면 바로 * 표시
                 result[i][j] = "*"
         else:
-            if arr[i][j] == "*" and bomb_check:
+            if arr[i][j] == "*" and bomb_check:  # 안 밟았어도 다른 지뢰를 밟으면 *로 출력
                 result[i][j] = "*"
 
 for i in range(n):
