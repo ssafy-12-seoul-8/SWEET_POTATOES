2d689d0d562360630df425d6f1233dffa57bd03b
junyoung <jylee0619@naver.com>
2025-01-31 15:59:55 +0900

    [Gold V] Title: 치킨 배달, Time: 128 ms, Memory: 111508 KB -SsafyHub

--- 이준영/백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 이준영/백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,40 +1,34 @@
-import sys
-input=sys.stdin.readline
-def calc():
-    global minimum
-    count=0
-    for i in house:
-        distance=101
-        for j in remain:
-            distance=min(distance,abs(i[0]-j[0])+abs(i[1]-j[1]))
-        count=count+distance
-    minimum=min(minimum,count)
-def choose(a,b):
-    if b==m:
-        calc()
-        return
-    elif a>l-1:
-        return
-    else:
-        f=remain[b]
-        remain[b]=chicken[a]
-        choose(a+1,b+1)
-        remain[b]=f
-        choose(a+1,b)
-n,m=map(int,input().split())
-minimum=int(1e9)
-c=[0]*n
-house=[]
-chicken=[]
-remain=[0]*m
-for i in range(n):
-    c[i]=list(map(int,input().split()))
-for i in range(n):
-    for j in range(n):
-        if c[i][j]==1:
-            house.append((i,j))
-        elif c[i][j]==2:
-            chicken.append((i,j))
-l=len(chicken)
-choose(0,0)
-print(minimum)
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+home = []
+chicken = []
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == 1:
+            home.append((i, j))
+        elif arr[i][j] == 2:
+            chicken.append((i, j))
+h_len = len(home)
+c_len = len(chicken)
+dis = [[0] * c_len for _ in range(h_len)]
+for i in range(h_len):
+    for j in range(c_len):
+        dis[i][j] = (abs(home[i][0] - chicken[j][0]) + abs(home[i][1] - chicken[j][1]), j)  # (거리, 치킨집인덱스)
+    dis[i].sort()
+
+mn = 100000
+for l in range(1 << c_len):  # 비트마스킹
+    cnt = 0
+    check = 0
+    for i in range(c_len):
+        if (l & (1 << i)) != 0:
+            check += 1
+    if check != M:           # 고른 치킨집이 M개 일 때만 체크
+        continue
+    for i in range(h_len):
+        for j in range(c_len):
+            if (l & (1 << dis[i][j][1])) != 0:  # 미리 정렬을 했기에 조건 만족하는 치킨집있으면 바로 반영
+                cnt = cnt + dis[i][j][0]
+                break
+    mn = min(mn, cnt)
+print(mn)
