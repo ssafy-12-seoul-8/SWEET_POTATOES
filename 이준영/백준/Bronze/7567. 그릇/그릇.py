ddfb8c58d2c1bb56b8dabd4f7d912aa92adb93d9
junyoung <jylee0619@naver.com>
2025-01-31 17:46:42 +0900

    [Bronze II] Title: 그릇, Time: 88 ms, Memory: 108384 KB -SsafyHub

--- 이준영/백준/Bronze/7567. 그릇/그릇.py
+++ 이준영/백준/Bronze/7567. 그릇/그릇.py
@@ -1,10 +1,10 @@
 str = input()
 cnt = 10
 cur = str[0]
-for i in range(1,len(str)):
-    if cur == str[i]:
-        cnt+=5
+for i in range(1, len(str)):
+    if cur == str[i]:           # 원래 그릇과 놓으려고 하는 그릇의 방향이 같다면 높이를 5만 더해준다.
+        cnt += 5
     else:
-        cnt+=10
-        cur = str[i]
+        cnt += 10               # 원래 그릇과 놓으려고 하는 그릇의 방향이 다르면 높이를 10 더해주고
+        cur = str[i]            # 최신 그릇의 상태를 수정
 print(cnt)
