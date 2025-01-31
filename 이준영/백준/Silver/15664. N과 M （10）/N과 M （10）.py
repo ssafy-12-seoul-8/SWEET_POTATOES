3e6c35a10e7592ee021e42483bbda2a6fbf0e09b
junyoung <jylee0619@naver.com>
2025-01-31 09:26:59 +0900

    [Silver II] Title: N과 M (10), Time: 92 ms, Memory: 108384 KB -SsafyHub

---
+++ 이준영/백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,27 @@
+def btk(cur, lst):
+    if len(lst) == M:
+        print(*lst)
+        return
+    if cur >= l:
+        return
+    for i in range(cur, l):
+        if dct[num[i]] >= 1:
+            dct[num[i]] -= 1
+            lst.append(num[i])
+            btk(i, lst)
+            lst.pop()
+            dct[num[i]] += 1
+
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+dct = {}
+for i in arr:
+    if i in dct:
+        dct[i] += 1
+    else:
+        dct[i] = 1
+num = list(dct.keys())
+num.sort()
+l = len(num)
+btk(0, [])
