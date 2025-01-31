7bb768324d75c8d80e785b148ee51207eb4be14a
junyoung <jylee0619@naver.com>
2025-01-31 16:23:24 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 88 ms, Memory: 108384 KB -SsafyHub

--- 이준영/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 이준영/백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,32 +1,13 @@
-import sys
-input=sys.stdin.readline
-n=int(input())
-home=[0]*n
-board=[0]*n
-for i in range(n):
-    home[i]=list(map(int,input().split()))
-    board[i]=[0]*n #가로 세로 대각으로 끝나는
-for i in range(n):
-    for j in range(n):
-        board[i][j]=[0,0,0]
-for i in range(1,n):
-    if home[0][i]==0:
-        board[0][i][0]=1
-    else:
-        break
-for i in range(1,n):
-    for j in range(2,n):
-        if home[i][j]==1:
-            continue
-        else:
-            board[i][j][0]=board[i][j-1][0]+board[i][j-1][2]
-            if i==1:
-                board[i][j][1]=0
-            else:
-                board[i][j][1]=board[i-1][j][1]+board[i-1][j][2]
-            if home[i][j-1]==1 or home[i-1][j]==1:
-                board[i][j][2]=0
-            else:
-                board[i][j][2]=board[i-1][j-1][0]+board[i-1][j-1][1]+board[i-1][j-1][2]
-c,d,e=board[n-1][n-1]
-print(c+d+e)
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0, 0, 0] for _ in range(N + 1)] for _ in range(N + 1)]  # 오른쪽 끝 기준으로 생각, 가로, 세로, 대각선
+dp[1][2][0] = 1
+for i in range(1, N + 1):
+    for j in range(3, N + 1):
+        if arr[i - 1][j - 1] == 0:
+            dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2]
+            dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2]
+            if i >= 2 and arr[i - 1][j - 2] == 0 and arr[i - 2][j - 1] == 0:
+                dp[i][j][2] = sum(dp[i - 1][j - 1])
+
+print(sum(dp[N][N]))
