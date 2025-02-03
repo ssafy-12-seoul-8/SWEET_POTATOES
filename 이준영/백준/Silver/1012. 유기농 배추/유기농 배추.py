d59ffbd10f4fc4e29822b2ab9383632c4e504e4b
junyoung <jylee0619@naver.com>
2025-02-03 11:53:59 +0900

    [Silver II] Title: 유기농 배추, Time: 128 ms, Memory: 111448 KB -SsafyHub

--- 이준영/백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 이준영/백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,30 +1,30 @@
 import sys
-from collections import deque
+
 input = sys.stdin.readline
 
 T = int(input())
-dy = [0,0,1,-1]
-dx = [1,-1,0,0]
+dy = [0, 0, 1, -1]  # 4방 탐색을 위한 dy
+dx = [1, -1, 0, 0]  # 4방 탐색을 위한 dx
 for tc in range(T):
     count = 0
-    M, N, K = map(int,input().split())
-    ground =[[0 for _ in range(M)]for _ in range(N)]
-    visited = [[False for _ in range(M)]for _ in range(N)]
+    M, N, K = map(int, input().split())
+    ground = [[0 for _ in range(M)] for _ in range(N)]  # 배추가 있는 곳
+    visited = [[False] * M for _ in range(N)]
     for i in range(K):
-        X,Y = map(int,input().split())
+        X, Y = map(int, input().split())
         ground[Y][X] = 1
     for i in range(N):
         for j in range(M):
-            if ground[i][j]==1:
-                count+=1
-                dq = deque([(i,j)])
-                while(dq):
-                    tmp = dq.popleft()
-                    for k in range(4):
-                        ny = tmp[0] + dy[k]
-                        nx = tmp[1] + dx[k]
-                        if(0<=ny<N and 0<=nx<M and ground[ny][nx]==1):
-                            ground[ny][nx] = 0
-                            dq.append([ny,nx])
+            if ground[i][j] == 1 and not visited[i][j]:
+                count += 1
+                stk = [(i, j)]
+                while stk:
+                    tmp = stk.pop()
+                    if not visited[tmp[0]][tmp[1]]:
+                        visited[tmp[0]][tmp[1]] = True
+                        for k in range(4):
+                            ny = tmp[0] + dy[k]
+                            nx = tmp[1] + dx[k]
+                            if 0 <= ny < N and 0 <= nx < M and ground[ny][nx] == 1 and not visited[ny][nx]:  # 범위 및 배추, 방문 여부
+                                stk.append((ny, nx))
     print(count)
-
