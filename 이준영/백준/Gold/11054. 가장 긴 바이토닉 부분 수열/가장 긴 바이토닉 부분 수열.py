6cc57a783dd4f233f26e84b534bf158c68b00cc1
junyoung <jylee0619@naver.com>
2025-02-03 16:12:01 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 164 ms, Memory: 110980 KB -SsafyHub

--- 이준영/백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 이준영/백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -1,19 +1,40 @@
 import sys
-input=sys.stdin.readline
-n=int(input())
-c=list(map(int,input().split()))
-d=list(reversed(c))
-e=[1]*n
-f=[1]*n
-m=0
-for i in range(n):
-    for j in range(0,i):
-        if c[i]>c[j]:
-            e[i]=max(e[i],e[j]+1)
-for i in range(n):
-    for j in range(0,i):
-        if d[i]>d[j]:
-            f[i]=max(f[i],f[j]+1)
-for i in range(n):
-    m=max(m,e[i]+f[n-i-1])
-print(m-1)
+
+
+def find(arr):
+    if not arr:
+        return 0
+    mx = arr[-1]
+    lst = []
+    for i in range(len(arr)):
+        if arr[i] >= mx:
+            continue
+        if not lst:
+            lst.append(arr[i])
+        elif lst[-1] < arr[i]:
+            lst.append(arr[i])
+        elif lst[0] > arr[i]:
+            lst[0] = arr[i]
+        else:
+            start = 0
+            end = len(lst) - 1
+            while start < end - 1:
+                mid = (start + end) // 2
+                if lst[mid] < arr[i]:
+                    start = mid
+                else:
+                    end = mid
+            lst[end] = arr[i]
+    return len(lst)
+
+
+input = sys.stdin.readline
+N = int(input())
+lst = list(map(int, input().split()))
+
+max_count = 0
+for i in range(N):
+    max1 = find(lst[:i + 1])
+    max2 = find(list(reversed(lst[i:])))
+    max_count = max(max_count, max1 + max2 + 1)
+print(max_count)
