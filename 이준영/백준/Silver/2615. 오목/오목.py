32be9be1ec6516c5777389721fbc9c581dd2efec
junyoung <jylee0619@naver.com>
2025-02-04 20:10:58 +0900

    [Silver I] Title: 오목, Time: 96 ms, Memory: 109544 KB -SsafyHub

--- 이준영/백준/Silver/2615. 오목/오목.py
+++ 이준영/백준/Silver/2615. 오목/오목.py
@@ -1,6 +1,6 @@
 arr = [list(map(int, input().split())) for _ in range(19)]
 win = 0
-dy = [-1, 0, 1, 1]
+dy = [-1, 0, 1, 1]          # 궁금한 것이 가장 왼쪽 돌의 좌표이므로 그 돌을 기준으로 4방향을 탐색하면 된다.
 dx = [1, 1, 1, 0]
 col = 0
 row = 0
@@ -15,7 +15,7 @@ for i in range(19):
 
         for k in range(4):
             cnt = 0
-            for l in range(1, 5):
+            for l in range(1, 5):                   # 오목이 완성되었는가를 판단
                 ny = i + dy[k] * l
                 nx = j + dx[k] * l
                 if 0 <= nx < 19 and 0 <= ny < 19:
@@ -23,8 +23,8 @@ for i in range(19):
                         cnt += 1
             if cnt < 4:
                 continue
-            else:
-                for a in [-1, 5]:
+            else:                                   # 오목이 완성되었다면
+                for a in [-1, 5]:                   # 6목이 아닌지를 판별해보고 검사한다.
                     ny = i + dy[k] * a
                     nx = j + dx[k] * a
                     if 0 <= nx < 19 and 0 <= ny < 19 and arr[ny][nx] == target:
