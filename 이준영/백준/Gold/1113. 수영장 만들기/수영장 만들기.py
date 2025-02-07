295c808f1e508b9027c19f514aba0dc2c32a00d5
junyoung <jylee0619@naver.com>
2025-02-07 09:08:30 +0900

    [Gold I] Title: 수영장 만들기, Time: 156 ms, Memory: 114936 KB -SsafyHub

--- 이준영/백준/Gold/1113. 수영장 만들기/수영장 만들기.py
+++ 이준영/백준/Gold/1113. 수영장 만들기/수영장 만들기.py
@@ -1,4 +1,3 @@
-# 0810~0816, 0819~
 import sys
 from collections import deque
 
@@ -6,23 +5,25 @@ input = sys.stdin.readline
 
 
 def find_h(start_y, start_x, height):
-    visited = [[False] * M for _ in range(N)]
     dq = deque([(start_y, start_x)])
-    cnt = 1
+    cnt = 0
+    check = True
     while dq:
         y, x = dq.popleft()
         if not visited[y][x]:
             visited[y][x] = True
-            if y == 0 or y == N - 1 or x == 0 or x == M - 1:
-                cnt = 0
-                break
+            cnt += 1
             for k in range(4):
                 ny = y + dy[k]
                 nx = x + dx[k]
-                if arr[ny][nx] < height and not visited[ny][nx]:
+                if 0 <= ny < N and 0 <= nx < M and arr[ny][nx] < height and not visited[ny][nx]:
                     dq.append((ny, nx))
+                    if ny == 0 or ny == N - 1 or nx == 0 or nx == M - 1:
+                        check = False
 
-    return cnt
+    if check:
+        return cnt
+    return 0
 
 
 N, M = map(int, input().split())
@@ -33,9 +34,10 @@ dx = [1, -1, 0, 0]
 tot = 0
 
 for h in range(2, 10):
+    visited = [[False] * M for _ in range(N)]
     for i in range(1, N - 1):
         for j in range(1, M - 1):
-            if arr[i][j] < h:
+            if arr[i][j] < h and not visited[i][j]:
                 tot += find_h(i, j, h)
 
 print(tot)
