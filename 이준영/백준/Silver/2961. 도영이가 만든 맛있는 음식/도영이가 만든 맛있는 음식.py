b6ca7a944220634dae0e4bf5d529610cc74540ff
junyoung <jylee0619@naver.com>
2025-02-10 11:58:43 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 92 ms, Memory: 109544 KB -SsafyHub

--- 이준영/백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 이준영/백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,20 +1,19 @@
-# 11:35 ~
-def btk(cur, s, b, check):
-    global mn
-
-    if cur == N:
-        if check:
-            mn = min(mn, abs(s - b))
-        return
-
-    btk(cur + 1, s, b, check)
-    btk(cur + 1, s * arr[cur][0], b + arr[cur][1], True)
-
+# 제출횟수: 회
+# 메모리: KB
+# 시간: ms
 
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-mn = abs(arr[0][0] - arr[0][1])
+mn = abs(arr[0][0] - arr[0][1])  # 초기값은 1번 재료만 사용했을 때로 지정
 
-btk(0, 1, 0, False)
+for i in range(1, 1 << N):
+    s = 1
+    b = 0
+    for j in range(N):
+        if i & (1 << j) != 0:
+            s = s * arr[j][0]
+            b = b + arr[j][1]
+
+    mn = min(mn, abs(s - b))
 
 print(mn)
