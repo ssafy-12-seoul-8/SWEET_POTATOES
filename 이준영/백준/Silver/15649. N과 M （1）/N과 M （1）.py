0322267b2c82a575ba48966195e1483f15a3ef7b
junyoung <jylee0619@naver.com>
2025-02-10 14:06:29 +0900

    [Silver III] Title: N과 M (1), Time: 172 ms, Memory: 114776 KB -SsafyHub

--- 이준영/백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 이준영/백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,14 +1,16 @@
-def btk(lst):
-    if len(lst)==M:
+# 백트래킹
+def btk(lst):                   # 현재 리스트
+    if len(lst) == M:
         print(*lst)
-    for i in range(1,N+1):
+    for i in range(1, N + 1):   # 범위에 있는 것중 방문하지 않은 것만 다음으로 넘어갈 수 있음
         if not visited[i]:
             visited[i] = True
             lst.append(i)
-            btk(lst)
+            btk(lst)            # 리스트에 추가하고 방문표시한 후 다시 풀어줘야 한다.
             lst.pop()
             visited[i] = False
 
-N, M = map(int,input().split())
-visited = [False] * (N+1)
+
+N, M = map(int, input().split())
+visited = [False] * (N + 1)     # 방문배열
 btk([])
