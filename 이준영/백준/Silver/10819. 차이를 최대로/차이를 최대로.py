9193d4d6b6d142d9db5abfa56a1e6ce245819a76
junyoung <jylee0619@naver.com>
2025-02-11 09:30:13 +0900

    [Silver II] Title: 차이를 최대로, Time: 148 ms, Memory: 111480 KB -SsafyHub

--- 이준영/백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 이준영/백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,9 +1,10 @@
-def btk(lst):
-    if len(lst)==N:
+# 1~N을 줄 세우기 (순열) 이후 그 리스트를 바탕으로 연산하여 최대값을 갱신하였습니다.
+def btk(lst):                   # 1~N 줄 세우기
+    if len(lst) == N:                   
         check(lst)
         return
 
-    for i in range(N):
+    for i in range(N):          # visited배열로 방문 여부를 관리하여 만듦
         if not visited[i]:
             visited[i] = True
             lst.append(i)
@@ -11,17 +12,19 @@ def btk(lst):
             visited[i] = False
             lst.pop()
 
-def check(lst):
+
+def check(lst):                 # 만든 리스트를 바탕으로 결과값을 계산하여 mx를 최신화
     global mx
 
     sm = 0
-    for i in range(N-1):
-        sm = sm + abs(arr[lst[i]]-arr[lst[i+1]])
+    for i in range(N - 1):
+        sm = sm + abs(arr[lst[i]] - arr[lst[i + 1]])
 
-    mx = max(sm,mx)
+    mx = max(sm, mx)
+
 
 N = int(input())
-arr = list(map(int,input().split()))
+arr = list(map(int, input().split()))
 mx = 0
 visited = [False] * N
 btk([])
