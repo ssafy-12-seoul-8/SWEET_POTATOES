5849f64b0ed7deb5e57727bd5aff4f4a1bf745e2
junyoung <jylee0619@naver.com>
2025-02-12 08:45:22 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 380 ms, Memory: 114996 KB -SsafyHub

--- 이준영/백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
+++ 이준영/백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -1,22 +1,22 @@
-def btk(cur, cnt):
+def btk(cur, cnt):                   # 현재 계란, 깨진 계란 수
     global mx
 
-    if 2 * (N - cur) + cnt < mx:
-        return
+    if 2 * (N - cur) + cnt < mx:     # 한번 내리칠 때마다 최대 2개씩 깨짐  
+        return                       # 다 깨도 최대에 못미침
 
     if cur == N:
         mx = max(mx, cnt)
         return
 
-    if arr[cur][0] <= 0:
+    if arr[cur][0] <= 0:             # 현재 계란이 깨짐
         btk(cur + 1, cnt)
         return
 
     s1, w1 = arr[cur]
-    check = False
+    check = False                    # 다른 계란 중 깨진게 있는가
 
     for i in range(N):
-        if i == cur or arr[i][0] <= 0:
+        if i == cur or arr[i][0] <= 0: 
             continue
 
         s2, w2 = arr[i]
@@ -34,7 +34,7 @@ def btk(cur, cnt):
         arr[cur][0] = s1
         arr[i][0] = s2
 
-    if check:
+    if check:                           # 하나라도 깨져 있다.
         return
 
     btk(cur + 1, cnt)
