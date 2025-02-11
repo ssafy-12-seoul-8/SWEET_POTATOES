08631d9a60e1d85cf30c78ed35679c18917391c0
junyoung <jylee0619@naver.com>
2025-02-11 19:49:30 +0900

    [Silver II] Title: 영재의 시험, Time: 296 ms, Memory: 113756 KB -SsafyHub

--- 이준영/백준/Silver/19949. 영재의 시험/영재의 시험.py
+++ 이준영/백준/Silver/19949. 영재의 시험/영재의 시험.py
@@ -1,14 +1,17 @@
-def btk(cur, cont, l_num, score):   # 몇번째, 연속한 횟수, 마지막 수, 점수
+def btk(cur, cont, l_num, score):  # 몇번째, 연속한 횟수, 마지막 수, 점수
     global cnt
 
-    if cur == 10:
+    if score - cur + 5 < 0:     # 다 맞아도 안됨
+        return
+    
+    if cur == 10:               # 끝까지 왔다.
         if score >= 5:
             cnt += 1
         return
 
-    if cont <= 1:
-        for i in range(1, 6):
-            tmp1 = cont
+    if cont <= 1:               # 연속한 횟수가 1이하이면 아무거나 와도 된다.
+        for i in range(1, 6):   
+            tmp1 = cont         # 연속한 횟수와 점수는 조건문으로 관리
             tmp2 = score
             if i == l_num:
                 tmp1 += 1
@@ -17,7 +20,7 @@ def btk(cur, cont, l_num, score):   # 몇번째, 연속한 횟수, 마지막 수
             if i == arr[cur]:
                 tmp2 += 1
             btk(cur + 1, tmp1, i, tmp2)
-    else:
+    else:                       # 연속한 횟수가 2면 무조건 이전 번호와 다르게 찍는다.
         for i in range(1, 6):
             if i == l_num:
                 continue
