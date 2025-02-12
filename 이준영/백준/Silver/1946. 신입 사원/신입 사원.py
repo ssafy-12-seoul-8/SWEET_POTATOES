fac8983c96b15cae774f580cdfe80c02775f0a81
junyoung <jylee0619@naver.com>
2025-02-12 15:32:39 +0900

    [Silver I] Title: 신입 사원, Time: 4792 ms, Memory: 215604 KB -SsafyHub

---
+++ 이준영/백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,16 @@
+# 15:20
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    score = [list(map(int, input().split())) for _ in range(N)]
+    score.sort()
+    cnt = 1
+    mx = score[0][1]
+
+    for i in range(1, N):
+        if mx < score[i][1]:
+            continue
+        cnt += 1
+        mx = score[i][1]
+
+    print(cnt)
