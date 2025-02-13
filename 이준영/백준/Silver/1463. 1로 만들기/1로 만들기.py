a090bd0eb491156677a0062e85c0d692ed113818
junyoung <jylee0619@naver.com>
2025-02-13 11:10:28 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 116200 KB -SsafyHub

--- 이준영/백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 이준영/백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,21 +1,33 @@
-from collections import deque
+def f(a):
+    if dp[a] != -1:
+        return dp[a]
+    if a == 0:
+        dp[0] = 1000000
+        return dp[0]
+
+    if a == 1:
+        dp[1] = 0
+        return 0
+
+    if a % 6 == 0:
+        dp[a] = min(f(a // 2), f(a // 3)) + 1
+        return dp[a]
+    if a % 6 == 2:
+        dp[a] = min(f(a // 2), f(a - 2) + 1) + 1
+        return dp[a]
+    if a % 6 == 3:
+        dp[a] = min(f(a // 3), f(a - 1)) + 1
+        return dp[a]
+    if a % 6 == 4:
+        dp[a] = min(f(a // 2), f(a - 1)) + 1
+        return dp[a]
+
+    dp[a] = f(a - 1) + 1
+    return dp[a]
+
 
 N = int(input())
 
-dp = [1000000] * (N + 1)
-dp[1] = 0
-dq = deque([1])
-while dq:
-    a = dq.popleft()
+dp = [-1] * (N + 1)
 
-    for i in (a + 1, 2 * a, 3 * a):
-        if i > N:
-            continue
-
-        if dp[i] <= dp[a] + 1:
-            continue
-
-        dp[i] = dp[a] + 1
-        dq.append(i)
-
-print(dp[N])
+print(f(N))
