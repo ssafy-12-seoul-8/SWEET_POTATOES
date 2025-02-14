abaad541996d70411042f7702bc237aaf8b24b8d
junyoung <jylee0619@naver.com>
2025-02-14 14:09:49 +0900

    [Gold IV] Title: 가르침, Time: 540 ms, Memory: 113600 KB -SsafyHub

--- 이준영/백준/Gold/1062. 가르침/가르침.py
+++ 이준영/백준/Gold/1062. 가르침/가르침.py
@@ -1,3 +1,7 @@
+# a,c,i,n,t는 반드시 들어가야 되기 때문에 K가 5이상이어야 한다.
+# K가 5이상일 때 미리 5개를 K에 넣어놓는다고 생각하고 K=K-5를 하며 
+# 각 단어의 집합에서도 위의 5글자는 빼고 관리한다.
+# 남은 것은 21개 중 K개를 뽑았을 때 cnt를 세서 가장 큰 값을 찾는다.
 import sys
 
 input = sys.stdin.readline
@@ -36,10 +40,10 @@ def check():
 
 N, K = map(int, input().split())
 arr = [set([]) for _ in range(N)]
-lst = [i for i in range(26) if i not in (0, 2, 8, 13, 19)]
+lst = [i for i in range(26) if i not in (0, 2, 8, 13, 19)]  # a,c,i,n,t아니면 넣는다
 dct = {}
 for i in range(21):
-    dct[lst[i]] = i
+    dct[lst[i]] = i                                         # a,c,i,n,t를 빼고 앞으로 땡긴 결과
 
 visited = [0] * 21
 
