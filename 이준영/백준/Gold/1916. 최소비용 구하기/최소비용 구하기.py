a97834513abf6fc2860c0f303b7a42214a9cad67
junyoung <jylee0619@naver.com>
2025-02-17 16:46:19 +0900

    [Gold V] Title: 최소비용 구하기, Time: 224 ms, Memory: 122964 KB -SsafyHub

--- 이준영/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 이준영/백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,25 +1,32 @@
-import heapq
+import sys
+from heapq import heappop, heappush
+
+input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
 road = [[] for _ in range(N + 1)]
+
 for _ in range(M):
     a, b, c = map(int, input().split())
-    road[a].append((b, c))
+    road[a].append((b, c))                                  # (목적지,거리)
 
 start, end = map(int, input().split())
 
 pq = []
-visited = [False] * (N + 1)
 distance = [int(1e9)] * (N + 1)
-heapq.heapify(pq)
-heapq.heappush(pq, (0, start))
+distance[start] = 0 
+heappush(pq, (0, start))
+
 while pq:
-    dis, des = heapq.heappop(pq)
-    if not visited[des]:
-        visited[des] = True
-        for tmp_des, tmp_dis in road[des]:
-            if not visited[tmp_des] and tmp_dis + dis < distance[tmp_des]:
-                distance[tmp_des] = tmp_dis + dis
-                heapq.heappush(pq, (distance[tmp_des], tmp_des))
+    dis, des = heappop(pq)
+
+    if distance[des] < dis:                                 # 이미 최적화 되어있다면 패스
+        continue
+
+    for tmp_des, tmp_dis in road[des]:
+        if dis + tmp_dis < distance[tmp_des]:               # 이 경로로 갔을 대 최적화가 된다면
+            distance[tmp_des] = dis + tmp_dis               # 거리를 갱신해주고
+            heappush(pq, (distance[tmp_des], tmp_des))      # 이 지점으로부터 다시 갱신해준다.
+
 print(distance[end])
