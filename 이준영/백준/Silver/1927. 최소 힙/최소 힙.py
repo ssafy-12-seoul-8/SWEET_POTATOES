6af4fa5b29ae7ebf1e1e991ac642be021dc47421
junyoung <jylee0619@naver.com>
2025-02-17 14:16:18 +0900

    [Silver II] Title: 최소 힙, Time: 160 ms, Memory: 114204 KB -SsafyHub

--- 이준영/백준/Silver/1927. 최소 힙/최소 힙.py
+++ 이준영/백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,15 +1,16 @@
 import sys
 import heapq
+
 input = sys.stdin.readline
-pq= []
+pq = []
 heapq.heapify(pq)
 N = int(input())
 for _ in range(N):
     x = int(input())
-    if x==0:
-        if not pq:
+    if x == 0:
+        if not pq:                      # 비어있으면 0을 출력
             print(0)
         else:
-            print(heapq.heappop(pq))
+            print(heapq.heappop(pq))    # 비어있지 않으면 빼면서 출력
     else:
-        heapq.heappush(pq,x)
+        heapq.heappush(pq, x)           # pq에 x 삽입
