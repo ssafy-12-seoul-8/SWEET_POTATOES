f564030cbccce1b415e99476ccc5a8ab4812d765
junyoung <jylee0619@naver.com>
2025-02-18 14:35:11 +0900

    [Bronze II] Title: 수 정렬하기, Time: 104 ms, Memory: 111124 KB -SsafyHub

--- 이준영/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 이준영/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,10 +1,11 @@
+# 퀵 정렬
 import sys
 
 input = sys.stdin.readline
 
 
-def quick(left, right):
-    if right - left <= 0:
+def quick(left, right):                 # arr[left:right+1]까지를 정렬하겠다.
+    if right - left <= 0:               # 길이가 0이거나 1이면 더 할 필요 없음
         return
 
     pivot = arr[left]
@@ -12,22 +13,22 @@ def quick(left, right):
     j = right
 
     while i < j:
-        while i < j and pivot > arr[i]:
+        while i < j and pivot > arr[i]:                         # i<j이면서 pivot보다 작으면 계속 더함
             i = i + 1
 
-        while i < j and pivot < arr[j]:
+        while i < j and pivot < arr[j]:                         # i<j이면서 pivot보다 크면 계속 뺌
             j = j - 1
 
-        arr[i], arr[j] = arr[j], arr[i]
-
-    if arr[i] < pivot:
-        arr[i], arr[left] = arr[left], arr[i]
+        arr[i], arr[j] = arr[j], arr[i]                         # 자리 바꿈
+                                        
+    if arr[i] < pivot:                                          # i위치에 있는게 pivot보다 작으면 바꾸면 됨
+        arr[i], arr[left] = arr[left], arr[i]   
         location = i
     else:
-        arr[i - 1], arr[left] = arr[left], arr[i - 1]
+        arr[i - 1], arr[left] = arr[left], arr[i - 1]           # i위치에 있는게 pivot보다 크면 i-1위치와 바꿔야 함
         location = i - 1
-
-    quick(left, location - 1)
+                                                                # location은 최종 pivot의 index
+    quick(left, location - 1)                                   # location을 기준으로 좌우로 정렬하면 끝
     quick(location + 1, right)
 
 
