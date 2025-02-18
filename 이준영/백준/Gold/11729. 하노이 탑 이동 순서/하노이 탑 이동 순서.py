5a43cad664008fd2497ead796eaa4ecd5cb7961b
junyoung <jylee0619@naver.com>
2025-02-18 16:19:00 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 424 ms, Memory: 149732 KB -SsafyHub

--- 이준영/백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 이준영/백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,17 +1,18 @@
-import sys
-
-def btk(n, start, end):                 # 옮길 개수, 시작 지점, 끝지점
-    if n == 1:
-        print(start, end)               # 하나면 바로 옮기면 된다.
+# 결국엔 N개의 온전한 탑을 start에서 end로 옮기기 위해서는
+# N-1개를 start에서 나머지 칸으로 옮기고 (칸이 1,2,3이니까 6-start-end를 하면 나머지 칸이 된다.)
+# 큰 원판을 start에서 end로 옮긴 후
+# N-1개를 마저 end로 옮기면 된다.
+def hanoi(cnt, start, end):
+    if cnt == 1:
+        print(start, end)
         return
 
-    btk(n - 1, start, 6 - start - end)  # 일단 n-1개를 나머지 하나로 옮기고
-    print(start, end)                   # 가장 밑의 원판은 마지막에 옮기고
-    btk(n - 1, 6 - start - end, end)    # n-1개의 원판을 다시 마저 옮긴다.
+    hanoi(cnt - 1, start, 6 - start - end)
+    print(start, end)
+    hanoi(cnt - 1, 6 - start - end, end)
 
 
 N = int(input())
+print((1 << N) - 1)
 
-print(2 ** N - 1)
-
-btk(N, 1, 3)
+hanoi(N, 1, 3)
