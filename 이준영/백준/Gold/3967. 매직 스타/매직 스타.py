6656d8812f18dbd929317ee3715fe2b649ef2f0c
junyoung <jylee0619@naver.com>
2025-02-20 20:01:46 +0900

    [Gold V] Title: 매직 스타, Time: 108 ms, Memory: 110956 KB -SsafyHub

--- 이준영/백준/Gold/3967. 매직 스타/매직 스타.py
+++ 이준영/백준/Gold/3967. 매직 스타/매직 스타.py
@@ -1,10 +1,13 @@
+# 미리 각 좌표를 지정하고 해결하였습니다.
+# 각 좌표가 어떤 줄에 속하는지, 어떤 알파벳이 쓰여있는지 등을 관리하였습니다.
+# 문제랑 다르게 A를 0으로 하였고 합을 22가 되게 하였습니다.
 def btk(cur):
-    global flag
+    global flag                             # 답을 찾았는가
 
     if flag:
         return
 
-    if cur == 12:
+    if cur == 12:                           # 다 채웠으니 ans배열에 답을 채워넣습니다.
         flag = True
         for i in range(5):
             for j in range(9):
@@ -12,25 +15,25 @@ def btk(cur):
         return
 
     y, x = lst[cur]
-    if arr[y][x] != "x":
+    if arr[y][x] != "x":                    # 이미 채워져있으면 넘어갑니다.
         btk(cur + 1)
         return
 
     for i in range(12):
-        if a_visited[i] == 0:
+        if a_visited[i] == 0:               # 만약 이 알파벳을 쓴적이 없다면
             t_flag = True
             for j in change[cur]:
-                if check[j][1] == 3 and (check[j][0] + i) != 22:
+                if check[j][1] == 3 and (check[j][0] + i) != 22:    # 각 줄에 대해 위배되는 사항이 있으면 안됨
                     t_flag = False
                     break
-            if t_flag:
-                a_visited[i] = 1
-                arr[y][x] = chr(ord("A") + i)
-                for j in change[cur]:
+            if t_flag:                      # 위배되는 사항이 없다면 넣어보자
+                a_visited[i] = 1            # 이 알파벳 방문
+                arr[y][x] = chr(ord("A") + i)   # 채워넣고
+                for j in change[cur]:       # 각 줄의 정보 갱신
                     check[j][1] += 1
                     check[j][0] += i
                 btk(cur+1)
-                a_visited[i] = 0
+                a_visited[i] = 0            # 그 다음 복구 작업
                 arr[y][x] = "x"
                 for j in change[cur]:
                     check[j][1] -= 1
