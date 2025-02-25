9bb625e3aa5e0d51257cac71a1ddf84c7ef5c0fd
junyoung <jylee0619@naver.com>
2025-02-25 13:57:48 +0900

    [Silver II] Title: 지구 온난화, Time: 88 ms, Memory: 108384 KB -SsafyHub

--- 이준영/백준/Silver/5212. 지구 온난화/지구 온난화.py
+++ 이준영/백준/Silver/5212. 지구 온난화/지구 온난화.py
@@ -1,33 +1,35 @@
+R, C = map(int, input().split())
+arr = [list(input()) for _ in range(R)]
+ans = [["."] * C for _ in range(R)]
 dy = [0, 0, 1, -1]
 dx = [1, -1, 0, 0]
-R, C = map(int, input().split())
-arr = [0] * R
-result = [["."] * C for _ in range(R)]
-for i in range(R):
-    arr[i] = list(input())
-min_row = 11
-min_col = 11
-max_row = 0
-max_col = 0
+
+mn_y = R
+mn_x = C
+mx_y = -1
+mx_x = -1
+
 for i in range(R):
     for j in range(C):
         if arr[i][j] == "X":
-            count = 0
+            cnt = 0
             for k in range(4):
                 ny = i + dy[k]
                 nx = j + dx[k]
                 if 0 <= ny < R and 0 <= nx < C:
                     if arr[ny][nx] == ".":
-                        count += 1
+                        cnt += 1
                 else:
-                    count += 1
-            if count <= 2:
-                result[i][j] = "X"
-                min_col = min(min_col, i)
-                max_col = max(max_col, i)
-                min_row = min(min_row, j)
-                max_row = max(max_row, j)
-for i in range(min_col, max_col + 1):
-    for j in range(min_row, max_row + 1):
-        print(result[i][j], end="")
+                    cnt += 1
+
+            if cnt <= 2:
+                ans[i][j] = "X"
+                mn_y = min(i, mn_y)
+                mn_x = min(j, mn_x)
+                mx_y = max(i, mx_y)
+                mx_x = max(j, mx_x)
+
+for i in range(mn_y, mx_y + 1):
+    for j in range(mn_x, mx_x + 1):
+        print(ans[i][j], end="")
     print()
