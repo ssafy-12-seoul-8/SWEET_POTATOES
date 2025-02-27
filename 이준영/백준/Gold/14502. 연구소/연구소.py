07b62467753e824babec817eb8cc30043ca35cf7
junyoung <jylee0619@naver.com>
2025-02-27 13:54:15 +0900

    [Gold IV] Title: 연구소, Time: 352 ms, Memory: 117152 KB -SsafyHub

--- 이준영/백준/Gold/14502. 연구소/연구소.py
+++ 이준영/백준/Gold/14502. 연구소/연구소.py
@@ -1,52 +1,65 @@
-def btk(cur, num):
-    if num == 3:
+from collections import deque
+
+
+def btk(cur, cnt):
+    if cnt == 3:
         check()
         return
-    if cur == N * M:
+
+    if cur == n * m:
         return
-    y = cur // M
-    x = cur % M
-    if arr[y][x] == 0:
-        arr[y][x] = 1
-        btk(cur + 1, num + 1)
-        arr[y][x] = 0
-    btk(cur + 1, num)
+
+    y = cur // m
+    x = cur % m
+
+    if arr[y][x] != 0:
+        btk(cur + 1, cnt)
+        return
+
+    btk(cur + 1, cnt)
+    arr[y][x] = 1
+    btk(cur + 1, cnt + 1)
+    arr[y][x] = 0
 
 
 def check():
-    global max_count
-    v_count = 0
-    visited = [[False] * M for _ in range(N)]
-    for start_y, start_x in lst:
-        if visited[start_y][start_x]:
+    global mx
+    visited = [[0] * m for _ in range(n)]
+    cnt = 0
+    for s_y, s_x in fire:
+        if visited[s_y][s_x] == 1:
             continue
-        stk = [(start_y, start_x)]
-        while stk:
-            y, x = stk.pop()
-            if not visited[y][x]:
-                visited[y][x] = True
-                v_count += 1
-                for k in range(4):
-                    ny = y + dy[k]
-                    nx = x + dx[k]
-                    if 0 <= ny < N and 0 <= nx < M and not visited[ny][nx] and arr[ny][nx] != 1:
-                        stk.append((ny, nx))
-    max_count = max(max_count, cnt - v_count)
+
+        visited[s_y][s_x] = 1
+        dq = deque([(s_y, s_x)])
+
+        while dq:
+            y, x = dq.popleft()
+            for k in range(4):
+                ny = y + dy[k]
+                nx = x + dx[k]
+                if 0 <= ny < n and 0 <= nx < m and arr[ny][nx] == 0 and visited[ny][nx] == 0:
+                    cnt += 1
+                    visited[ny][nx] = 1
+                    dq.append((ny, nx))
+
+    mx = max(mx, tot - cnt)
 
 
-N, M = map(int, input().split())
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]  # 2가 불, 1이 방화벽, 0이 빈칸
 dy = [0, 0, 1, -1]
 dx = [1, -1, 0, 0]
-arr = [list(map(int, input().split())) for _ in range(N)]
-cnt = -3
-lst = []
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 0:
-            cnt += 1
-        elif arr[i][j] == 2:
-            cnt += 1
-            lst.append((i, j))
-max_count = 0
+fire = []
+tot = -3
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 2:
+            fire.append((i, j))
+        elif arr[i][j] == 0:
+            tot += 1
+
+mx = 0
 btk(0, 0)
-print(max_count)
+
+print(mx)
