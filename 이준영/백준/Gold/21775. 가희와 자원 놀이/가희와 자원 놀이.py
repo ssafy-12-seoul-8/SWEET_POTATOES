942ac79bae902074fcd787e7992ae7d2a703abc1
junyoung <jylee0619@naver.com>
2025-03-05 08:48:09 +0900

    [Gold IV] Title: 가희와 자원 놀이, Time: 1032 ms, Memory: 338360 KB -SsafyHub

--- 이준영/백준/Gold/21775. 가희와 자원 놀이/가희와 자원 놀이.py
+++ 이준영/백준/Gold/21775. 가희와 자원 놀이/가희와 자원 놀이.py
@@ -1,26 +1,27 @@
+# 제출횟수: 1회
+# 메모리: 455832KB
+# 시간: 1712ms
+# 각 인원의 set과 전체 set을 따로 관리해서 문제를 풀었지만 문제의 제한 사항을 보니 안그래도 되더라...
 import sys
 
 input = sys.stdin.readline
 
 
-def do(s, target):
-
-    if s[1] == "next":
-        own[target] = -1
+def do(s, target):                                              # s명령을 target이 실행
+    if s[1] == "next":  
+        own[target] = -1                                        # 카드 버리기
         return
 
     n = int(s[2])
-    if s[1] == "acquire":
-        if n in tot_set:
+    if s[1] == "acquire":                                       
+        if n in tot_set:                                        # 다른 사람 자원에 있으면 아무것도 안함
             return
-        else:
+        else:                                                   # 없으면 가져온다.
             tot_set.add(n)
-            num[target].add(n)
             own[target] = -1
             return
 
-    tot_set.remove(n)
-    num[target].remove(n)
+    tot_set.remove(n)                                           # 다 아니면 n을 제거
     own[target] = -1
 
 
@@ -36,7 +37,6 @@ for i in range(T):
 
 idx = 0
 
-num = [set([]) for _ in range(N + 1)]  # i번이 소지한 자원
 tot_set = set([])  # 전체가 소지한 자원
 own = [-1] * (N + 1)  # 내가 소유한 연산카드 인덱스
 
