445353dfc0ee86060ef8b93ea9f22a10a6e3cdfd
junyoung <jylee0619@naver.com>
2025-03-05 13:53:15 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 128 ms, Memory: 111480 KB -SsafyHub

--- 이준영/백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
+++ 이준영/백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.py
@@ -1,25 +1,24 @@
 def R(arr):
-    mx_len = -1
     lst = [[] for _ in range(N)]
+    mx_len = -1
     for i in range(N):
         cnt = [0] * 101
         for j in arr[i]:
-            if j == 0:
-                continue
-            else:
-                cnt[j] += 1
-        t_lst = []
+            cnt[j] += 1
+
+        tmp_lst = []
         for j in range(1, 101):
             if cnt[j] != 0:
-                t_lst.append([cnt[j], j])
+                tmp_lst.append((cnt[j], j))
 
-        t_lst.sort()
-        for a, b in t_lst:
+        tmp_lst.sort()
+        for a, b in tmp_lst:
             lst[i].append(b)
             lst[i].append(a)
+
         mx_len = max(mx_len, len(lst[i]))
 
-    mx_len = min(mx_len, 100)
+    mx_len = min(100, mx_len)
     new_arr = [[0] * mx_len for _ in range(N)]
     for i in range(N):
         for j in range(min(100, len(lst[i]))):
@@ -29,23 +28,23 @@ def R(arr):
 
 
 def C(arr):
-    mx_len = -1
     lst = [[] for _ in range(M)]
+    mx_len = -1
     for j in range(M):
         cnt = [0] * 101
         for i in range(N):
-            if arr[i][j] == 0:
-                continue
             cnt[arr[i][j]] += 1
 
-        t_lst = []
+        tmp_lst = []
         for i in range(1, 101):
             if cnt[i] != 0:
-                t_lst.append([cnt[i], i])
-        t_lst.sort()
-        for a, b in t_lst:
+                tmp_lst.append((cnt[i], i))
+
+        tmp_lst.sort()
+        for a, b in tmp_lst:
             lst[j].append(b)
             lst[j].append(a)
+
         mx_len = max(mx_len, len(lst[j]))
 
     mx_len = min(100, mx_len)
@@ -58,15 +57,16 @@ def C(arr):
 
 
 r, c, k = map(int, input().split())
-A = [list(map(int, input().split())) for _ in range(3)]
+
 r = r - 1
 c = c - 1
+
+A = [list(map(int, input().split())) for _ in range(3)]
+
 N = 3
 M = 3
-
-t = 0
 ans = -1
-
+t = 0
 while t <= 100:
     if r <= N - 1 and c <= M - 1 and A[r][c] == k:
         ans = t
@@ -76,6 +76,7 @@ while t <= 100:
         A = R(A)
     else:
         A = C(A)
+
     N = len(A)
     M = len(A[0])
     t += 1
