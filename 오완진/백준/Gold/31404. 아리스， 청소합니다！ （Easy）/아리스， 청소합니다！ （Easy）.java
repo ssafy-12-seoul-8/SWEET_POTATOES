5fb871bd519cee036305348942760d940dbaf670
WJ <130027416+OiPKL@users.noreply.github.com>
2025-03-10 22:13:49 +0900

    [Gold II] Title: 아리스, 청소합니다! (Easy), Time: 224 ms, Memory: 12308 KB -SsafyHub

--- 오완진/백준/Gold/31404. 아리스， 청소합니다！ （Easy）/아리스， 청소합니다！ （Easy）.java
+++ 오완진/백준/Gold/31404. 아리스， 청소합니다！ （Easy）/아리스， 청소합니다！ （Easy）.java
@@ -8,22 +8,27 @@ public class Main {
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, 1, 0, -1};
 	
+	static int R, C, rNow, cNow, dNow, rNext, cNext, visitCnt = 0, cleanCnt = 0, emptyCnt = 0, totalCnt = 0;
+	static boolean[][] cleaned;
+	static int[][][] visited;
+	static int[][] ruleA, ruleB;
+	
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
-        int R = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
         
         st = new StringTokenizer(br.readLine());
-        int rNow = Integer.parseInt(st.nextToken());
-        int cNow = Integer.parseInt(st.nextToken());
-        int dNow = Integer.parseInt(st.nextToken());
+        rNow = Integer.parseInt(st.nextToken());
+        cNow = Integer.parseInt(st.nextToken());
+        dNow = Integer.parseInt(st.nextToken());
         
-        boolean[][] cleaned = new boolean[R][C];
-        boolean[][][] visited = new boolean[R][C][4];
-        int[][] ruleA = new int[R][C];
-        int[][] ruleB = new int[R][C];
+        cleaned = new boolean[R][C];
+        visited = new int[R][C][4];
+        ruleA = new int[R][C];
+        ruleB = new int[R][C];
         
         for (int r = 0; r < R; r++) {
         	String line = br.readLine();
@@ -36,32 +41,28 @@ public class Main {
         	for (int c = 0; c < C; c++)
         		ruleB[r][c] = line.charAt(c) - '0';
         }
-
-        int cleanCnt = 0;
-        int emptyCnt = 0;
-        int totalCnt = 0;
         
         while (true) {
         	
         	if (!cleaned[rNow][cNow]) {
         		cleaned[rNow][cNow] = true;
-        		visited = new boolean[R][C][4];
+        		visitCnt++;
         		emptyCnt = 0;
         		dNow = (dNow + ruleA[rNow][cNow]) % 4;
         	} else {
         		
-                if (visited[rNow][cNow][dNow]) {
+                if (visited[rNow][cNow][dNow] == visitCnt) {
                 	totalCnt += cleanCnt - emptyCnt;
                 	break;
                 }
                 
-                visited[rNow][cNow][dNow] = true;
+                visited[rNow][cNow][dNow] = visitCnt;
         		emptyCnt++;
         		dNow = (dNow + ruleB[rNow][cNow]) % 4;
         	}
         	
-        	int rNext = rNow + dr[dNow];
-        	int cNext = cNow + dc[dNow];
+        	rNext = rNow + dr[dNow];
+        	cNext = cNow + dc[dNow];
         	cleanCnt++;
         	
             if (rNext < 0 || R <= rNext || cNext < 0 || C <= cNext) {
