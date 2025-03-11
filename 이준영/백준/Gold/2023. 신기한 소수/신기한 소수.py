3e35787a3ee69960e09ab20109f74be1d130af44
junyoung <jylee0619@naver.com>
2025-03-11 19:37:39 +0900

    [Gold V] Title: 신기한 소수, Time: 88 ms, Memory: 109544 KB -SsafyHub

--- 이준영/백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 이준영/백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,9 +1,11 @@
+# 에라토스테네스의 체를 쓰기에는 최대 8자리까지 가능하기 때문에 시간이 너무 많이 든다.
+# 따라서 순차적으로 늘려나가는 방식 채택
 import math
 
 
-def check_prime(n):
+def check_prime(n):                 # 소수인가
     tmp = int(math.sqrt(n))
-    for i in range(2, tmp + 1):
+    for i in range(2, tmp + 1):     # 루트n까지만 보면 된다.
         if n % i == 0:
             return False
 
@@ -19,9 +21,9 @@ else:
     for l in range(N - 1):
         tmp_lst = []
         for num in lst:
-            for i in (1, 3, 5, 7, 9):
+            for i in (1, 3, 7, 9):  # 뒷자리에는 1,3,7,9만 올 수 있다.
                 tmp = num * 10 + i
-                if check_prime(tmp):
+                if check_prime(tmp):# 소수면 저장하자
                     tmp_lst.append(tmp)
         lst = tmp_lst
 
