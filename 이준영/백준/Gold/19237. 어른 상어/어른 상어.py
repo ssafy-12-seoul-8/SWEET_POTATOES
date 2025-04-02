e55a63eba91d770e7aa5863cc09eb5711a480251
junyoung <jylee0619@naver.com>
2025-04-02 15:31:02 +0900

    [Gold II] Title: 어른 상어, Time: 192 ms, Memory: 112052 KB -SsafyHub

--- 이준영/백준/Gold/19237. 어른 상어/어른 상어.py
+++ 이준영/백준/Gold/19237. 어른 상어/어른 상어.py
@@ -1,92 +1,87 @@
-from collections import deque
+def myprint():
+    print("board")
+    for i in range(n):
+        print(*arr[i])
 
-N, M, K = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-direction = list(map(int, input().split()))
+    print("-" * 50)
+
+
+def oob(y, x):
+    return not (0 <= y < n and 0 <= x < n)
+
+
 dy = [0, -1, 1, 0, 0]
 dx = [0, 0, 0, -1, 1]
-ord = [0] * (M + 1)
-
-for i in range(1, M + 1):
-    ord[i] = [list(map(int, input().split())) for _ in range(4)]
-
-smell_map = [[set([]) for _ in range(N)] for _ in range(N)]
-shark_deque = [deque([]) for _ in range(M + 1)]
-shark_dct = [{} for _ in range(M + 1)]
-
-for i in range(N):
-    for j in range(N):
+n, m, K = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+direction = list(map(int, input().split()))
+order = [[]] + [[[]] + [list(map(int, input().split())) for _ in range(4)] for _ in range(m)]
+ground = [[[0, 0] for _ in range(n)] for _ in range(n)]
+player_loc = [0] * (m + 1)
+for i in range(n):
+    for j in range(n):
         if arr[i][j] != 0:
-            num = arr[i][j]
-            shark_deque[num].append((i, j))
-            smell_map[i][j].add(num)
-            arr[i][j] = [arr[i][j], direction[num - 1]]
-            shark_dct[num][(i, j)] = 0
-        else:
-            arr[i][j] = [0, 0]
+            tmp = arr[i][j]
+            ground[i][j] = [tmp, K]
+            player_loc[tmp] = [i, j, direction[tmp - 1]]
 
-time = 0
+turn = 0
 ans = -1
+cnt = m
+while turn <= 999:
+    turn += 1
+    tmp_arr = [[0] * n for _ in range(n)]
 
-while time < 1000:
-    time += 1
-    shark_map = [[[0, 0] for _ in range(N)] for _ in range(N)]
-    t_loc = []
+    for i in range(1, m + 1):
+        if player_loc[i][0] == -1:
+            continue
 
-    for i in range(N):
-        for j in range(N):
-            num = arr[i][j][0]
-            if num > 0:
-                flag = False
+        flag = False
+        y, x, d = player_loc[i]
+        for k in order[i][d]:
+            ny = y + dy[k]
+            nx = x + dx[k]
+            if not oob(ny, nx) and ground[ny][nx][0] == 0:
+                flag = True
+                if tmp_arr[ny][nx] == 0:
+                    tmp_arr[ny][nx] = i
+                    player_loc[i] = [ny, nx, k]
+                else:
+                    cnt -= 1
+                    player_loc[i] = [-1, -1, -1]
+                break
+        if flag:
+            continue
 
-                for k in ord[num][arr[i][j][1] - 1]:
-                    ny = i + dy[k]
-                    nx = j + dx[k]
+        for k in order[i][d]:
+            ny = y + dy[k]
+            nx = x + dx[k]
+            if not oob(ny, nx) and ground[ny][nx][0] == i:
+                if tmp_arr[ny][nx] == 0:
+                    tmp_arr[ny][nx] = i
+                    player_loc[i] = [ny, nx, k]
+                else:
+                    cnt -= 1
+                    player_loc[i] = [-1, -1, -1]
+                break
 
-                    if 0 <= ny < N and 0 <= nx < N and not smell_map[ny][nx]:
-                        flag = True
-                        t_loc.append((num, ny, nx))
-                        if not (0 < shark_map[ny][nx][0] < num):
-                            shark_map[ny][nx] = [num, k]
-                        break
+    for i in range(n):
+        for j in range(n):
+            if ground[i][j][1] > 0:
+                ground[i][j][1] -= 1
+                if ground[i][j][1] == 0:
+                    ground[i][j] = [0, 0]
 
-                if flag:
-                    continue
+    for i in range(1, m + 1):
+        if player_loc[i][0] != -1:
+            y, x, d = player_loc[i]
+            ground[y][x] = [i, K]
 
-                for k in ord[num][arr[i][j][1] - 1]:
-                    ny = i + dy[k]
-                    nx = j + dx[k]
-
-                    if 0 <= ny < N and 0 <= nx < N and num in smell_map[ny][nx]:
-                        t_loc.append((num, ny, nx))
-                        if not (0 < shark_map[ny][nx][0] < num):
-                            shark_map[ny][nx] = [num, k]
-                        break
-
-    if time >= K:
-        for i in range(1, M + 1):
-            if shark_deque[i]:
-                y, x = shark_deque[i].popleft()
-                if shark_dct[i][(y, x)] == time - K:
-                    smell_map[y][x].remove(i)
-                    del shark_dct[i][(y, x)]
-
-    for num, y, x in t_loc:
-        smell_map[y][x].add(num)
-        shark_dct[num][(y, x)] = time
-        shark_deque[num].append((y, x))
-
-    cnt = 0
-    for i in range(N):
-        for j in range(N):
-            num = shark_map[i][j][0]
-            if num > 0:
-                cnt += 1
+    arr = tmp_arr
 
     if cnt == 1:
-        ans = time
+        ans = turn
         break
 
-    arr = shark_map
 
 print(ans)
