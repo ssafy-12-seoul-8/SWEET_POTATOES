94600227f1e5d6ee7791d11ad60760dc8a03f7aa
Ryuharam <dlfnadl97@naver.com>
2025-05-15 23:47:58 +0900

    [level 0] Title: 문자열 돌리기, Time: 298.47 ms, Memory: 71.2 MB -SsafyHub

---
+++ 유하람/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(char c : a.toCharArray()){
+            System.out.println(c);
+        }
+    }
+}
