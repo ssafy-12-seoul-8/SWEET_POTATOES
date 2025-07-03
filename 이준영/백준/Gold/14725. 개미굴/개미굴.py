a8d5f096d61036228847bb595407262d02a2f8ed
junyoung <jylee0619@naver.com>
2025-07-03 15:56:35 +0900

    [Gold III] Title: 개미굴, Time: 108 ms, Memory: 237868 KB -SsafyHub

--- 이준영/백준/Gold/14725. 개미굴/개미굴.py
+++ 이준영/백준/Gold/14725. 개미굴/개미굴.py
@@ -1,12 +1,15 @@
 import sys
-sys.setrecursionlimit(10**5)
+
+sys.setrecursionlimit(10 ** 5)
 input = sys.stdin.readline
+
 
 class Node:
     def __init__(self, name=""):
         self.name = name
-        self.child = {}      # key: 문자열, value: Node
-        self.depth = 0       # 루트 바로 아래 자식이 depth=1
+        self.child = {}  # key: 문자열, value: Node
+        self.depth = 0  # 루트 바로 아래 자식이 depth=1
+
 
 class Trie:
     def __init__(self):
@@ -14,13 +17,14 @@ class Trie:
 
     def insert(self, parts):
         node = self.root
-        for d, part in enumerate(parts, start=1):
+        for d in range(len(parts)):
+            part = parts[d]
             if part not in node.child:
                 new_node = Node(part)
-                new_node.depth = d
+                new_node.depth = d + 1
                 node.child[part] = new_node
             node = node.child[part]
-            node.depth = d
+            node.depth = d + 1
 
     def dfs_print(self, node):
         # 자식 키를 사전 순으로 정렬하여 출력
