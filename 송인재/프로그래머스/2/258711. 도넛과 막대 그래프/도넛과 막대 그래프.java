a49a0f22bc82e6fbb0e944324531f3aec0f2c978
Injae Song <110002292+IjjS@users.noreply.github.com>
2025-10-05 20:34:45 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 292.37 ms, Memory: 272 MB -SsafyHub

--- 송인재/프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 송인재/프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -6,7 +6,6 @@ class Solution {
     int[] exits;
     int max;
     int[] result;
-    boolean[] visited;
     
     public int[] solution(int[][] edges) {
         graph = new HashMap<>();
@@ -14,6 +13,10 @@ class Solution {
         exits = new int[1_000_001];
         max = 0;
         result = new int[4];
+        
+        for (int i = 0; i < enters.length; i++) {
+            enters[i] = -1;
+        }
         
         for (int[] edge : edges) {
             int from = edge[0];
@@ -23,55 +26,49 @@ class Solution {
             graph.get(from)
                 .add(to);
             
+            if (enters[to] < 0) {
+                enters[to] = 0;
+            }
+            
             enters[to]++;
             exits[from]++;
             
             max = Math.max(max, Math.max(from, to));
         }
-        
+
         result[0] = getCreator();
-        visited = new boolean[max + 1];
+        int totalCount = exits[result[0]];
         
-        for(int to : graph.get(result[0])) {
+        for (int to : graph.get(result[0])) {
             enters[to]--;
-            
-            int type = dfs(to);
-            
-            result[type]++;
         }
+        
+        for (int i = 1; i <= max; i++) {
+            if (i == result[0]) {
+                continue;
+            }
+            
+            if (enters[i] == 2 && exits[i] == 2) {
+                result[3]++;
+            }
+            
+            if (exits[i] == 0 && enters[i] >= 0) {
+                result[2]++;
+            }
+        }
+        
+        result[1] = totalCount - result[3] - result[2];
         
         return result;
     }
     
     int getCreator() {
         for (int i = 1; i <= max; i++) {
-            if (enters[i] == 0 && exits[i] >= 2) {
+            if (enters[i] == -1 && exits[i] >= 2) {
                 return i;
             }
         }
         
         return 0;
-    }
-    
-    int dfs(int curr) {
-        if (enters[curr] == 2 && exits[curr] == 2) {
-            return 3;
-        }
-        
-        if (exits[curr] == 0) {
-            return 2;
-        }
-        
-        visited[curr] = true;
-        
-        for (int to : graph.get(curr)) {
-            if (visited[to]) {
-                continue;
-            }
-            
-            return dfs(to);
-        }
-        
-        return 1;
     }
 }
