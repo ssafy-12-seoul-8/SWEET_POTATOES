80c5dfc14636ce0695955c2e01e0f91491e3eb67
Injae Song <110002292+IjjS@users.noreply.github.com>
2025-11-02 12:03:58 +0900

    [Gold IV] Title: 알파벳, Time: 2240 ms, Memory: 302144 KB -SsafyHub

---
+++ 송인재/백준/Gold/1987. 알파벳/README.md
@@ -0,0 +1,34 @@
+# [Gold IV] 알파벳 - 1987 
+
+[문제 링크](https://www.acmicpc.net/problem/1987) 
+
+### 성능 요약
+
+메모리: 302144 KB, 시간: 2240 ms
+
+### 분류
+
+그래프 이론, 그래프 탐색, 깊이 우선 탐색, 백트래킹, 격자 그래프
+
+### 제출 일자
+
+2025년 11월 2일 12:03:05
+
+### 문제 설명
+
+<p>A single-player game is played on a rectangular board divided in R rows and C columns. There is a single uppercase letter (A-Z) written in every position in the board. </p>
+
+<p>Before the begging of the game there is a figure in the upper-left corner of the board (first row, first column). In every move, a player can move the figure to the one of the adjacent positions (up, down, left or right). Only constraint is that a figure cannot visit a position marked with the same letter twice. The goal of the game is to play as many moves as possible. </p>
+
+<p>Write a program that will calculate the maximal number of positions in the board the figure can visit in a single game. </p>
+
+### 입력 
+
+ <p>The first line of the input file contains two integers R and C, separated by a single blank character, 1 ≤ R, S ≤ 20. </p>
+
+<p>The following R lines contain S characters each. Each line represents one row in the board. </p>
+
+### 출력 
+
+ <p>The first and only line of the output file should contain the maximal number of position in the board the figure can visit. </p>
+
---
+++ 송인재/백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  static int[] dr = {-1, 1, 0, 0};
+  static int[] dc = {0, 0, -1, 1};
+  static int r;
+  static int c;
+  static char[][] board;
+  static boolean[][] visited;
+  static Set<Character> set;
+  static int max;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    r = Integer.parseInt(st.nextToken());
+    c = Integer.parseInt(st.nextToken());
+    board = new char[r][c];
+    visited = new boolean[r][c];
+    set = new HashSet<>();
+
+    for (int i = 0; i < r; i++) {
+      board[i] = br.readLine()
+          .toCharArray();
+    }
+
+    set.add(board[0][0]);
+    visited[0][0] = true;
+    max = 1;
+
+    combination(0, 0, 1);
+
+    System.out.println(max);
+  }
+
+  static void combination(int row, int col, int current) {
+    for (int i = 0; i < 4; i++) {
+      int newRow = row + dr[i];
+      int newCol = col + dc[i];
+
+      if (isOutOfBox(newRow, newCol) || visited[newRow][newCol] || set.contains(board[newRow][newCol])) {
+        continue;
+      }
+
+      visited[newRow][newCol] = true;
+      set.add(board[newRow][newCol]);
+      max = Math.max(max, current + 1);
+
+      combination(newRow, newCol, current + 1);
+
+      visited[newRow][newCol] = false;
+      set.remove(board[newRow][newCol]);
+    }
+  }
+
+  static boolean isOutOfBox(int row, int col) {
+    return row < 0 || row >= r || col < 0 || col >= c;
+  }
+
+}
