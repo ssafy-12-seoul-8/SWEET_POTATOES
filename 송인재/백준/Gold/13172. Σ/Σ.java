9846a60cf1723c3acb8792dfbf90c625eba678a6
Injae Song <110002292+IjjS@users.noreply.github.com>
2025-11-04 15:45:26 +0900

    [Gold IV] Title: Σ, Time: 188 ms, Memory: 19852 KB -SsafyHub

---
+++ 송인재/백준/Gold/13172. Σ/Σ.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  static int P = 1_000_000_007;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int m = Integer.parseInt(br.readLine());
+    long numer = 0;
+    long denumer = 1;
+
+    for (int i = 0; i < m; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int s = Integer.parseInt(st.nextToken());
+      numer = s * denumer + n * numer;
+      denumer *= n;
+      numer %= P;
+      denumer %= P;
+    }
+
+    long sum = numer % denumer == 0 ? numer / denumer : numer * getInverse(denumer) % P;
+    System.out.println(sum);
+  }
+
+  static int getInverse(long base) {
+    long inverse = 1;
+    int exp = P - 2;
+
+    while (exp > 0) {
+      if ((exp & 1) == 1) {
+        inverse = inverse * base % P;
+      }
+
+      base = base * base % P;
+      exp >>= 1;
+    }
+
+    return (int) inverse;
+  }
+
+}
