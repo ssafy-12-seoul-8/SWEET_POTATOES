c340090e2f140329b3bec6eed6768734ffe63073
Injae Song <110002292+IjjS@users.noreply.github.com>
2025-11-06 16:27:58 +0900

    [Gold III] Title: 웜홀, Time: 432 ms, Memory: 29744 KB -SsafyHub

--- 송인재/백준/Gold/1865. 웜홀/웜홀.java
+++ 송인재/백준/Gold/1865. 웜홀/웜홀.java
@@ -4,29 +4,28 @@ import java.util.*;
 public class Main {
 
   static class Edge {
+    int from;
     int to;
     int weight;
 
-    Edge(int to, int weight) {
+    Edge(int from, int to, int weight) {
+      this.from = from;
       this.to = to;
       this.weight = weight;
     }
   }
 
-  static int INF = 25_000_000;
-
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int cases = Integer.parseInt(br.readLine());
+    int tc = Integer.parseInt(br.readLine());
     StringBuilder sb = new StringBuilder();
 
-    for (int t = 1; t <= cases; t++) {
+    for (int t = 0; t < tc; t++) {
+      List<Edge> edges = new ArrayList<>();
       StringTokenizer st = new StringTokenizer(br.readLine());
       int n = Integer.parseInt(st.nextToken());
       int m = Integer.parseInt(st.nextToken());
       int w = Integer.parseInt(st.nextToken());
-      int[][] dp = new int[n + 1][n + 1];
-      Map<Integer, List<Edge>> graph = new HashMap<>();
 
       for (int i = 0; i < m; i++) {
         st = new StringTokenizer(br.readLine());
@@ -34,12 +33,8 @@ public class Main {
         int to = Integer.parseInt(st.nextToken());
         int weight = Integer.parseInt(st.nextToken());
 
-        graph.putIfAbsent(from, new ArrayList<>());
-        graph.putIfAbsent(to, new ArrayList<>());
-        graph.get(from)
-            .add(new Edge(to, weight));
-        graph.get(to)
-            .add(new Edge(from, weight));
+        edges.add(new Edge(from, to, weight));
+        edges.add(new Edge(to, from, weight));
       }
 
       for (int i = 0; i < w; i++) {
@@ -48,50 +43,41 @@ public class Main {
         int to = Integer.parseInt(st.nextToken());
         int weight = Integer.parseInt(st.nextToken());
 
-        graph.putIfAbsent(from, new ArrayList<>());
-        graph.get(from)
-            .add(new Edge(to, -weight));
+        edges.add(new Edge(from, to, -weight));
       }
 
-      for (int[] row : dp) {
-        Arrays.fill(row, INF);
-      }
+      int[] dist = new int[n + 1];
 
-      for (int i = 1; i <= n; i++) {
-        if (!graph.containsKey(i)) {
-          continue;
-        }
+      for (int i = 0; i < n - 1; i++) {
+        boolean isUpdated = false;
 
-        for (Edge edge : graph.get(i)) {
-          if (dp[i][edge.to] > edge.weight) {
-            dp[i][edge.to] = edge.weight;
+        for (Edge edge : edges) {
+          if (dist[edge.to] > dist[edge.from] + edge.weight) {
+            dist[edge.to] = dist[edge.from] + edge.weight;
+            isUpdated = true;
           }
         }
-      }
 
-      for (int k = 1; k <= n; k++) {
-        for (int i = 1; i <= n; i++) {
-          for (int j = 1; j <= n; j++) {
-            dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
-          }
+        if (!isUpdated) {
+          break;
         }
       }
 
-      sb.append(isPossible(n, dp))
+      boolean hasNegative = false;
+
+      for (Edge edge : edges) {
+        if (dist[edge.to] > dist[edge.from] + edge.weight) {
+          hasNegative = true;
+
+          break;
+        }
+      }
+
+      sb.append(hasNegative ? "YES" : "NO")
           .append(System.lineSeparator());
     }
 
     System.out.print(sb);
-  }
-
-  static String isPossible(int n, int[][] dp) {
-    for (int i = 1; i <= n; i++) {
-      if (dp[i][i] < 0) {
-        return "YES";
-      }
-    }
-
-    return "NO";
   }
 
 }
