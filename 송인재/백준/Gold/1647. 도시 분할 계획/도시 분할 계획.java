cef544adea78dfdce9c8c6715df5f7b477a04f06
Injae Song <110002292+IjjS@users.noreply.github.com>
2025-11-09 09:37:29 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1644 ms, Memory: 331000 KB -SsafyHub

---
+++ 송인재/백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  static class Edge {
+    int from;
+    int to;
+    int weight;
+
+    Edge(int from, int to, int weight) {
+      this.from = from;
+      this.to = to;
+      this.weight = weight;
+    }
+  }
+
+  static int[] rep;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    List<Edge> edges = new ArrayList<>();
+    rep = new int[n + 1];
+    int max = 0;
+    int sum = 0;
+
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int from = Integer.parseInt(st.nextToken());
+      int to = Integer.parseInt(st.nextToken());
+      int weight = Integer.parseInt(st.nextToken());
+
+      edges.add(new Edge(from, to, weight));
+    }
+
+    for (int i = 1; i <= n; i++) {
+      rep[i] = i;
+    }
+
+    edges.sort(Comparator.comparingInt(e -> e.weight));
+
+    for (int i = 0; i < m; i++) {
+      Edge edge = edges.get(i);
+
+      if (union(edge.from, edge.to)) {
+        sum += edge.weight;
+        max = Math.max(max, edge.weight);
+      }
+    }
+
+    System.out.println(sum - max);
+  }
+
+  static int find(int x) {
+    if (rep[x] != x) {
+      rep[x] = find(rep[x]);
+    }
+
+    return rep[x];
+  }
+
+  static boolean union(int x, int y) {
+    int repX = find(x);
+    int repY = find(y);
+
+    if (repX == repY) {
+      return false;
+    }
+
+    rep[repY] = repX;
+
+    return true;
+  }
+
+}
