c627066213cf5bed67571bd35d11b5711f3900cf
Injae Song <110002292+IjjS@users.noreply.github.com>
2025-11-29 16:49:26 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 196 ms, Memory: 14676 KB -SsafyHub

--- 송인재/백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
+++ 송인재/백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.java
@@ -4,29 +4,20 @@ import java.util.*;
 public class Main {
 
   static String input;
-  static boolean[][] pal;
+  static int[] dp;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     input = br.readLine();
-    pal = new boolean[input.length()][input.length()];
-    int[] dp = new int[input.length() + 1];
-
-    for (int i = 0; i < input.length(); i++) {
-      findPal(i, i);
-      findPal(i, i + 1);
-    }
+    dp = new int[input.length() + 1];
 
     Arrays.fill(dp, 2_500);
 
     dp[0] = 0;
 
     for (int i = 0; i < input.length(); i++) {
-      for (int j = 0; j <= i; j++) {
-        if (pal[j][i]) {
-          dp[i + 1] = Math.min(dp[i + 1], dp[j] + 1);
-        }
-      }
+      findPal(i, i);
+      findPal(i, i + 1);
     }
 
     System.out.println(dp[input.length()]);
@@ -34,7 +25,7 @@ public class Main {
 
   static void findPal(int left, int right) {
     while (left >= 0 && right < input.length() && input.charAt(left) == input.charAt(right)) {
-      pal[left][right] = true;
+      dp[right + 1] = Math.min(dp[right + 1], dp[left] + 1);
       left--;
       right++;
     }
