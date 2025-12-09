25b7b1e1682680209638d8b607741f58614b355d
Injae Song <110002292+IjjS@users.noreply.github.com>
2025-12-09 09:20:11 +0900

    [Gold IV] Title: 직사각형 탈출, Time: 680 ms, Memory: 125600 KB -SsafyHub

--- 송인재/백준/Gold/16973. 직사각형 탈출/직사각형 탈출.java
+++ 송인재/백준/Gold/16973. 직사각형 탈출/직사각형 탈출.java
@@ -3,73 +3,13 @@ import java.util.*;
 
 public class Main {
 
-  static class Rectangle {
-    int r;
-    int c;
-    int h;
-    int w;
-    int dist;
-
-    Rectangle(int h, int w, int r, int c, int dist) {
-      this.r = r;
-      this.c = c;
-      this.h = h;
-      this.w = w;
-      this.dist = dist;
-      visited[r][c] = true;
-    }
-
-    boolean isValid(int direction) {
-      int row = r + dr[direction];
-      int col = c + dc[direction];
-      
-      if (isOutOfMap(row, col) || visited[row][col]) {
-        return false;
-      }
-
-      if (direction % 2 == 0) {
-        int nextRow = direction / 2 == 0 ? row : row + h;
-
-        for (int i = 0; i <= w; i++) {
-          int nextCol = col + i;
-
-          if (isOutOfMap(nextRow, nextCol) || map[nextRow][nextCol] == 1) {
-            return false;
-          }
-        }
-      } else {
-        int nextCol = direction / 2 == 0 ? col : col + w;
-
-        for (int i = 0; i <= h; i++) {
-          int nextRow = row + i;
-
-          if (isOutOfMap(nextRow, nextCol) || map[nextRow][nextCol] == 1) {
-            return false;
-          }
-        }
-      }
-
-      return true;
-    }
-
-    boolean isOutOfMap(int row, int col) {
-      return row < 0 || col < 0 || row >= n || col >= m;
-    }
-
-    Rectangle move(int direction) {
-      int nextRow = r + dr[direction];
-      int nextCol = c + dc[direction];
-
-      return new Rectangle(h, w, nextRow, nextCol, dist + 1);
-    }
-
-  }
-
   static int n;
   static int m;
   static int[][] map;
+  static int[][] prefix;
   static boolean[][] visited;
   static int[] dest;
+  static int[] hw;
   static int[] dr = { -1, 0, 1, 0 };
   static int[] dc = { 0, -1, 0, 1 };
 
@@ -79,9 +19,11 @@ public class Main {
     n = Integer.parseInt(st.nextToken());
     m = Integer.parseInt(st.nextToken());
     map = new int[n][m];
+    prefix = new int[n + 1][m + 1];
     visited = new boolean[n][m];
     dest = new int[2];
-    Queue<Rectangle> queue = new LinkedList<>();
+    hw = new int[2];
+    Queue<int[]> queue = new LinkedList<>();
 
     for (int i = 0; i < n; i++) {
       st = new StringTokenizer(br.readLine());
@@ -91,34 +33,59 @@ public class Main {
       }
     }
 
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        prefix[i][j] = map[i - 1][j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
+      }
+    }
+
     st = new StringTokenizer(br.readLine());
-    int h = Integer.parseInt(st.nextToken()) - 1;
-    int w = Integer.parseInt(st.nextToken()) - 1;
+    hw[0] = Integer.parseInt(st.nextToken()) - 1;
+    hw[1] = Integer.parseInt(st.nextToken()) - 1;
     int r = Integer.parseInt(st.nextToken()) - 1;
     int c = Integer.parseInt(st.nextToken()) - 1;
     dest[0] = Integer.parseInt(st.nextToken()) - 1;
     dest[1] = Integer.parseInt(st.nextToken()) - 1;
     int max = 0;
+    visited[r][c] = true;
 
-    queue.add(new Rectangle(h, w, r, c, 0));
+    queue.add(new int[]{ r, c, 0 });
 
     while (!queue.isEmpty()) {
-      Rectangle current = queue.poll();
+      int[] current = queue.poll();
+      int row = current[0];
+      int col = current[1];
 
       for (int i = 0; i < 4; i++) {
-        if (!current.isValid(i)) {
+        int rowStart = row + dr[i];
+        int rowEnd = rowStart + hw[0];
+        int colStart = col + dc[i];
+        int colEnd = colStart + hw[1];
+
+        if (isOutOfMap(rowStart, colStart) || visited[rowStart][colStart] || isOutOfMap(rowEnd, colEnd)) {
           continue;
         }
 
-        Rectangle next = current.move(i);
+        int wallSum = prefix[rowEnd + 1][colEnd + 1]
+            - prefix[rowEnd + 1][colStart]
+            - prefix[rowStart][colEnd + 1]
+            + prefix[rowStart][colStart];
 
-        if (next.r == dest[0] && next.c == dest[1]) {
-          max = next.dist;
+        if (wallSum != 0) {
+          continue;
+        }
+
+        visited[rowStart][colStart] = true;
+
+        int nextDist = current[2] + 1;
+
+        if (rowStart == dest[0] && colStart == dest[1]) {
+          max = nextDist;
 
           break;
         }
 
-        queue.add(next);
+        queue.add(new int[]{ rowStart, colStart, nextDist });
       }
 
       if (max != 0) {
@@ -127,6 +94,10 @@ public class Main {
     }
 
     System.out.println(max == 0 ? -1 : max);
+  }
+
+  static boolean isOutOfMap(int row, int col) {
+    return row < 0 || col < 0 || row >= n || col >= m;
   }
 
 }
